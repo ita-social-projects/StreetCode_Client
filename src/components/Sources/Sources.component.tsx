f98f19ff0330c2ea60603ef02a0ad0f01012bb82
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2022-12-27 19:41:09 +0200

    Added "Source" block component

--- src/components/Sources/Sources.component.tsx
+++ src/components/Sources/Sources.component.tsx
@@ -1,6 +1,8 @@
-import SlickSliderTest from "../../pages/SlickSlider/SlickSliderTest";
+import SlickSliderTest from "../../pages/SlickSlider/SlickSliderBase";
 import SourcesSlideItem from "../../pages/SlickSlider/SliderItems/SourcesSliderItem/SourcesSliderItem";
 import "./Sources.styles.css"
+import BlockHeading from "../../pages/BlockHeading/BlockHeading.component";
+let Left = require('../../assets/images/LeftDefaultSliderArrow.svg');
 
 interface Props {
 
@@ -13,16 +15,16 @@ const SourcesComponent = ({  }: Props) => {
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 1
-    }
+        slidesToScroll: 1,
+}
     return (
         <div className={"sourcesContainer"}>
-            <h1>Test</h1>
-            <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+            <BlockHeading headingText={"Для фанатів"}/>
+            <div style={{height:"250px", display:"flex", alignItems:"center", justifyContent:"center"}}>
              <div className={"sourcesSliderContainer"}>
-                    <SlickSliderTest {...settings}   slides={[<SourcesSlideItem text={"slide1"}/>,<SourcesSlideItem text={"slide2"}/>,<SourcesSlideItem text={"slide3"}/>,<SourcesSlideItem text={"slide4"}/>]}/>
+                    <SlickSliderTest {...settings}   slides={[<SourcesSlideItem text={"Книги"}/>,<SourcesSlideItem text={"Фільми"}/>,<SourcesSlideItem text={"Статті"}/>,<SourcesSlideItem text={"slide4"}/>]}/>
              </div>
-        </div>
+         </div>
         </div>
     );
 }
--- src/pages/SlickSlider/SlickSliderBase.tsx
+++ src/pages/SlickSlider/SlickSliderBase.tsx
@@ -1,12 +1,24 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./SlickSlider.styles.css"
 
 import React, {Component, useState} from "react";
 import Slider, {Settings as SliderProps} from "react-slick";
 
-interface Props extends SliderProps{
-    slides: JSX.Element[];
+const defaultProps: SliderProps = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    lazyLoad: "ondemand",
+    centerMode: true,
+    autoplay: true,
 }
+
+type Props = { slides: JSX.Element[] } & typeof defaultProps;
+
 
 
 const SimpleSlider = (props: Props) => {
