1333f2a79d730871f54039bfcfa0e6cc3a8edc5d
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2022-12-28 18:22:13 +0200

    Update SlickSliderBase.tsx

--- src/pages/SlickSlider/SlickSliderBase.tsx
+++ src/pages/SlickSlider/SlickSliderBase.tsx
@@ -1,42 +1,39 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import "./SlickSlider.styles.css"
 
-import React, {Component, useState} from "react";
-import Slider, {Settings as SliderProps} from "react-slick";
+import { FC } from "react"
+import Slider, { Settings as SliderProps } from "react-slick"
+
+interface Props extends SliderProps {
+    slides: JSX.Element[]
+}
+
+const SimpleSlider: FC<Props> = (props) => {
+    console.log(props)
+
+    return (
+        <div className={"sliderClass"} style={{width: "100%"}}>
+            <Slider {...props}>
+                {props.slides.map(slide => (
+                    <div className={'sdg'}>{slide}</div>
+                ))}
+            </Slider>
+        </div>
+    );
+}
 
 const defaultProps: SliderProps = {
     dots: true,
     arrows: true,
     infinite: true,
-    speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    lazyLoad: "ondemand",
-    centerMode: true,
-    autoplay: true,
+    lazyLoad: 'ondemand',
+    speed: 5e2,
+    autoplay: false,
+    autoplaySpeed: 4e3,
 }
+SimpleSlider.defaultProps = defaultProps
 
-type Props = { slides: JSX.Element[] } & typeof defaultProps;
-
-
-
-const SimpleSlider = (props: Props) => {
-
-        return (
-            <div style={{width:"100%"}} >
-                <Slider {...props}>
-                    {props.slides.map(slide =>
-                        (
-                            <div>{slide}</div>
-                        )
-                    )}
-
-
-                </Slider>
-            </div>
-        );
-    }
-
-    export default SimpleSlider
-
+export default SimpleSlider
