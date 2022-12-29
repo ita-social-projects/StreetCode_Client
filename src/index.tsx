104f0d6853423b5b2a1001d963d28dd0c6b880ee
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2022-12-29 16:59:21 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import 'assets/fonts/fonts.styles.css';
+import '@fonts/fonts.styles.css';
+
 import { RouterProvider } from "react-router-dom";
-import router from "./app/router/Routes";
+import router from "@app/router/Routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
