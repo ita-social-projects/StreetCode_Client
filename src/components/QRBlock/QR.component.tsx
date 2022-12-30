5c64d8a27e6b2c42b9de980fc7ade3a644653b53
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2022-12-30 17:01:52 +0200

    Update QR.component.tsx

--- src/components/QRBlock/QR.component.tsx
+++ src/components/QRBlock/QR.component.tsx
@@ -1,14 +1,26 @@
 import "./QR.styles.scss"
-const bg = require("assets/images/QRbackground.png")
-
+let IPhone = require("../../assets/images/QR_Iphone.png");
+let QR = require("../../assets/images/QR.png");
 interface Props {
 
 }
 
-
 const QRComponent = ({  }: Props) => {
     return (
         <div className={"QRBlockContainer"}>
+            <div className={"QRBlockContent"}>
+                <div className={"QRBlockImg"}>
+                    <img src={IPhone} />
+                </div>
+                <div className={"QRBlockText"}>
+                    <div className={"QRBlockTextWrapper"}>
+                    <h1>AR-історія в Інсті!</h1>
+                    <p>Переходь за QR-кодом, фокусуй камеру <br/>на ілюстрації та вітай нову реальність.</p>
+                    <p className={"p_bold"}>Скануй, щоб завіртуалити історію!</p>
+                    <img src={QR}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
