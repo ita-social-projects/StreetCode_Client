8d441e2d3e341982835bdc7c660f14a87b75b7ae
dimasster <dmytrobuchkovsky@gmail.com>
2023-01-02 18:19:58 +0200

    add event on slide click

--- src/features/RelatedFiguresBlock/RelatedFigureItem/RelatedFigureItem.tsx
+++ src/features/RelatedFiguresBlock/RelatedFigureItem/RelatedFigureItem.tsx
@@ -1,19 +1,27 @@
 import Tag from "@/models/additional-content/tag.model";
+import { Console } from "console";
 import "./RelatedFigureItem.styles.scss";
 
 interface Props {
-    TextHeading:string,
-    ImageSrc:string,
-    Tags:Tag[]
+    Id: number,
+    TextHeading: string,
+    ImageSrc: string,
+    Tags: Tag[]
+}
+
+const redirectOnStreetcode = (id: number) => {
+    console.log('redirected to streetcode with id: ' + id);
 }
 
 const RelatedFigureSliderItem = (props: Props) => {
     return (
-        <div className={"relatedFigureSlide"} style={{
-                backgroundImage: "url("+props.ImageSrc+")"}}>
+        <div 
+            className={"relatedFigureSlide"}
+            style={{backgroundImage: "url("+props.ImageSrc+")"}}
+            onClick={()=>{redirectOnStreetcode(props.Id)}}>
             <div className={"slideText"}>
                 <h3 className={"heading"}>{props.TextHeading}</h3>
-                <div className={"tagList"}>
+                <div className={"relatedTagList"}>
                     {props.Tags.map(tag =>(
                         <div className="tag">
                             <p>{tag.title}</p>
--- src/models/streetcode/related-figure.model.ts
+++ src/models/streetcode/related-figure.model.ts
@@ -2,6 +2,7 @@ import Tag from "../additional-content/tag.model"
 import Image from "../media/image.model"
 
 export default interface RelatedFigure {
+    Id: number;
     Title: string;
     Image: Image;
     Tags: Tag[];
