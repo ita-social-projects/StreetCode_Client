d7cbd2e3cc25107322eb84b27ae2843db2eec9bf
Alexander Lazarenko <ferrosful@gmail.com>
2023-01-02 19:28:20 +0200

    Added base header layout.

--- src/components/StreetcodeComponent/Streetcode.component.tsx
+++ src/components/StreetcodeComponent/Streetcode.component.tsx
@@ -5,6 +5,7 @@ import InterestingFactsComponent from "../InterestingFacts/InterestingFacts.comp
 import QRComponent from "../QRBlock/QR.component"
 import MainBlock from "@features/MainBlock/MainBlock.component";
 import TextComponent from "@features/TextBlock/Text.component";
+import HeaderBlock from "@features/HeaderBlock/HeaderBlock.component";
 
 interface Props {
 
@@ -12,6 +13,7 @@ interface Props {
 
 const StreetcodeContent = (props: Props) => (
     <div className='streetcodeContainer'>
+        <HeaderBlock />
         <MainBlock />
         <TextComponent/>
         <InterestingFactsComponent />
