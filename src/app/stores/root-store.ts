bc7ca1215af005607df72491f5f995379862523e
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-01-03 16:39:10 +0200

    Update root-store.ts

--- src/app/stores/root-store.ts
+++ src/app/stores/root-store.ts
@@ -1,15 +1,18 @@
 import { createContext, useContext } from "react";
 import SourcesStore from "@stores/sources-store";
 import InterestingFactsStore from "@stores/interesting-facts-store";
+import SubtitlesStore from "@stores/subtitles-store";
 
 interface Store {
     sourcesStore: SourcesStore,
-    interestingFactsStore: InterestingFactsStore
+    interestingFactsStore: InterestingFactsStore,
+    subtitlesStore: SubtitlesStore
 }
 
 export const store: Store = {
     sourcesStore: new SourcesStore(),
-    interestingFactsStore: new InterestingFactsStore()
+    interestingFactsStore: new InterestingFactsStore(),
+    subtitlesStore: new SubtitlesStore()
 }
 
 export const StoreContext = createContext(store);
