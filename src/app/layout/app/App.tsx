121449d55359220366f547a2e10c21cb711fa9b7
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-01-03 16:39:08 +0200

    Update App.tsx

--- src/app/layout/app/App.tsx
+++ src/app/layout/app/App.tsx
@@ -6,19 +6,27 @@ import { Outlet } from "react-router-dom";
 import SourcesModal from "@common/components/modals/Sources/SourcesModal.component";
 import InterestingFactsModal from "@common/components/modals/InterestingFacts/InterestingFactsModal.component";
 import Footer from '../footer/Footer.component';
+import {useAsync} from "@common/hooks/stateful/useAsync.hook";
+import {useMobx} from "@stores/root-store";
 
-const App = () => (
-    <>
-        <ToastContainer position='bottom-right' />
-        {
-            <>
-                <InterestingFactsModal/>
-                <SourcesModal />
-                <Header />
-                <Outlet />
-            </>
-        }
-    </>
-);
+const App = () => {
+    const {subtitlesStore:{getAll, getSubtitlesArray}} = useMobx();
+    const {value} = useAsync(getAll)
+    console.log(value);
+
+    return (
+        <>
+            <ToastContainer position='bottom-right'/>
+            {
+                <>
+                    <InterestingFactsModal/>
+                    <SourcesModal/>
+                    <Header/>
+                    <Outlet/>
+                </>
+            }
+        </>
+    );
+};
 
 export default App;
