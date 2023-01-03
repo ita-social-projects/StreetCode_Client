7d398047d3decb31b6d2884daab317ff3919c528
dimasster <dmytrobuchkovsky@gmail.com>
2023-01-03 16:07:55 +0200

    fix block representation for less then 4 elements

--- src/features/RelatedFiguresBlock/RelatedFigures.component.tsx
+++ src/features/RelatedFiguresBlock/RelatedFigures.component.tsx
@@ -19,9 +19,9 @@ interface Props {
 var relatedFigures: RelatedFigure[] = [
     {Id:1,Title:"Володимир Антонович",Image:{id:1,url:{id:1,href:Antonovich}},Tags:[{id:1,title:"Наукова школа"}, {id:1,title:"tag"}]}
     ,{Id:2,Title:"Леся Українка",Image:{id:1,url:{id:1,href:Ukrainka}},Tags:[{id:1,title:"tag"}]}
-    ,{Id:3,Title:"Іван Мазепа",Image:{id:1,url:{id:1,href:Mazepa}},Tags:[{id:1,title:"tag"}]}
-    ,{Id:4,Title:"Роман Ратушний",Image:{id:1,url:{id:1,href:Ratushny}},Tags:[{id:1,title:"tag"}]}
-    ,{Id:5,Title:"Богдан хмельницький",Image:{id:1,url:{id:1,href:Khmelnytsky}},Tags:[{id:1,title:"tag"}]}
+    //,{Id:3,Title:"Іван Мазепа",Image:{id:1,url:{id:1,href:Mazepa}},Tags:[{id:1,title:"tag"}]}
+    //,{Id:4,Title:"Роман Ратушний",Image:{id:1,url:{id:1,href:Ratushny}},Tags:[{id:1,title:"tag"}]}
+    //,{Id:5,Title:"Богдан хмельницький",Image:{id:1,url:{id:1,href:Khmelnytsky}},Tags:[{id:1,title:"tag"}]}
     ]
 
 const RelatedFiguresComponent = (props: Props) => {
@@ -36,22 +36,23 @@ const RelatedFiguresComponent = (props: Props) => {
     ))
 
     return (
-        <div className='relatedFiguresWrapper' style={{height: relatedFigures.length>4?'pxToRem(681px)':'pxToRem(613px)'}}>
+        <div 
+            className={'relatedFiguresWrapper ' + (relatedFigures.length>4?'bigWrapper':'smallWrapper')}>
             <div className='relatedFiguresContainer'>
                 <BlockHeading headingText="Зв'язки історії" />
-                <div className='relatedFiguresSliderContainer'>
+                <div className={'relatedFiguresSliderContainer'}>
                     <div style={{height: "100%"}}>
                         <SlickSlider
-                            className='heightContainer'
-                            slidesToShow={Math.min(relatedFigures.length,4)}
+                            className={'heightContainer'}
+                            slidesToShow={4}
                             slides={sliderItems}
                             swipe={false}
                             dots={false}
                         />
                     </div>
                 </div>
-                <div className="moreInfo" style={{visibility: relatedFigures.length>4?'visible':'hidden'}}>
-                    <a>Дивитися всіх</a>
+                <div className="moreInfo">
+                    <p>Дивитися всіх</p>
                 </div>
             </div>
         </div>
