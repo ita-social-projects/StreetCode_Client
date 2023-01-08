fa16c2187bd91e0ae58fe0e4d0583ed71648b3a0
Alexander Lazarenko <ferrosful@gmail.com>
2023-01-08 15:41:47 +0200

    Changed sources styles.

--- src/features/StreetcodePage/SourcesBlock/Sources.component.tsx
+++ src/features/StreetcodePage/SourcesBlock/Sources.component.tsx
@@ -9,34 +9,33 @@ import { useParams } from 'react-router-dom';
 
 const SourcesComponent = () => {
     const { sourcesStore } = useMobx();
+
     const { fetchSrcCategoriesByStreetcodeId, getSrcCategoriesArray } = sourcesStore;
 
     const { id: streetcodeId } = useParams<{ id: string }>();
     useAsync(() => fetchSrcCategoriesByStreetcodeId(parseInt(streetcodeId ?? '1')));
 
     return (
-        <>
-            <div className={"sourcesWrapper"}>
-                <div className={"sourcesContainer"}>
-                    <BlockHeading headingText={"Для фанатів"} />
-                    <div className={"sourceContentContainer"}>
-                        <div className={"sourcesSliderContainer"}>
-                            <SlickSlider
-                                swipeOnClick={false}
-                                swipe={false}
-                                dots={false}
-                                slides={getSrcCategoriesArray.flatMap(i => [i, i]).map(sc => (
-                                    <SourceItem
-                                        key={sc.id}
-                                        srcCategory={sc}
-                                    />
-                                ))}
-                            />
-                        </div>
+        <div className={'sourcesWrapper'}>
+            <div className={'sourcesContainer'}>
+                <BlockHeading headingText={'Для фанатів'}/>
+                <div className={'sourceContentContainer'}>
+                    <div className={'sourcesSliderContainer'}>
+                        <SlickSlider
+                            swipeOnClick={false}
+                            swipe={false}
+                            dots={false}
+                            slides={getSrcCategoriesArray.map(sc => (
+                                <SourceItem
+                                    key={sc.id}
+                                    srcCategory={sc}
+                                />
+                            ))}
+                        />
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
