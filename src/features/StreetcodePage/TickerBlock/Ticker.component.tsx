9e23c32dc5c1871eeac10253ca83d09112d69ad4
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-01-09 14:22:16 +0200

    Sync ticker with api

--- src/features/StreetcodePage/TickerBlock/Ticker.component.tsx
+++ src/features/StreetcodePage/TickerBlock/Ticker.component.tsx
@@ -1,19 +1,38 @@
 import "./Ticker.styles.scss";
 import Ticker from 'react-awesome-ticker';
+import Subtitle from "@models/additional-content/subtitles.model";
+import { useAsync } from '@/app/common/hooks/stateful/useAsync.hook';
+import { useParams } from 'react-router-dom';
+import SubtitlesApi from "@api/additional-content/subtitles.api";
+import {SubtitleStatus} from "@models/additional-content/subtitles.model";
 
-const tickerItems = [
-    "укладачі Інна Крупник, Оксана Юркова,",
-    "редакторка Ім`я Прізвище,",
-    "ілюстратор Сергій Федоров,","дизайнери Марія Кравцова, Олександр Івашко,",
-    "актор дубляжу Ім`я Прізвище,"
-];
+function createSubtitleString(subtitles:Subtitle[]){
+    let tickerItems:string[] = [];
+    subtitles.forEach((item)=>{
+        let tickerItem:string;
+        let subtitleStatus = Object.values(SubtitleStatus)[item.subtitleStatus];
+        tickerItem = subtitleStatus + " " + item.firstName + " " + item.lastName + ", ";
+        tickerItems.push(tickerItem);
+    })
+    return tickerItems;
+}
 
-const TickerComponent = () => (
-    <Ticker className={"tickerContainer"}>
-        {tickerItems.map((item, idx) => (
-            <div key={idx} className={"tickerItem"}>{item}</div>
-        ))}
-    </Ticker>
-)
+const TickerComponent = () => {
+    const streetcodeId = useParams<{id: string}>();
+    const id = parseInt(streetcodeId.id ?? "1");
+    const {value} = useAsync(() =>SubtitlesApi.getAll());
+    let subtitles = value as Subtitle[];
+    subtitles = (subtitles || []).filter((element) => {
+        return element.streetcodeId===id;
+    });
+    return (
+        <Ticker className={"tickerContainer"}>
+            {createSubtitleString(subtitles).map((item, idx) => (
+                <div key={idx} className={"tickerItem"}>{item}</div>
+            ))}
+        </Ticker>
+    );
+
+}
 
 export default TickerComponent;
