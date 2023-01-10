e58788c7b80c070c0e6ac880fde0525bfdd0070f
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-01-10 15:32:11 +0200

    Update InterestingFactsModal.component.tsx

--- src/app/common/components/modals/InterestingFacts/InterestingFactsModal.component.tsx
+++ src/app/common/components/modals/InterestingFacts/InterestingFactsModal.component.tsx
@@ -1,18 +1,19 @@
 import './InterestingFactsModal.styles.scss';
 import CancelBtn from "@assets/images/utils/Cancel_btn.svg";
-
+import factsApi from "@api/streetcode/text-content/facts.api";
+import {Fact} from "@models/streetcode/text-contents.model";
 import { Modal } from "antd";
+
 import useMobx from "@stores/root-store";
 import { observer } from "mobx-react-lite";
 
-const text = `7 (20) березня члени Центральної Ради обрали Михайла Грушевського своїм головою.
-\nРішення було прийняте без відома самого Грушевського, що свідчить про його колосальний авторитет.
-\n    На той час Грушевський навіть знаходився поза Україною, але повернувся, щоб обійняти посаду.
-\n    longTextlongTextlongTextlongTextlongTextlongTextlongTextlongTextlongTextlongTextlongTextlongText`;
-
 const InterestingFactsModal = () => {
-    const { modalStore } = useMobx();
+    const { factsStore:{ factMap },modalStore } = useMobx();
     const { setModal, modalsState: { facts } } = modalStore;
+    const { getByStreetcodeId } = factsApi;
+
+    const factId = facts.fromCardId!;
+    const fact = factMap.get(factId);
 
     return (
         <Modal className={"interestingFactsModal"}
@@ -27,9 +28,9 @@ const InterestingFactsModal = () => {
 
             </div>
             <div className={"factsContentContainer"}>
-                <h1>Голова Центральної Ради</h1>
+                <h1>{fact?.title}</h1>
                 <div className={"factsTextContainer"}>
-                    {text}
+                    {fact?.factContent}
                 </div>
             </div>
         </Modal>
