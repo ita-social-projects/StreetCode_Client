476335667d52bea42c8432d302768236c4f7dc6e
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-01-11 10:19:57 +0200

    Update Ticker.component.tsx

--- src/features/StreetcodePage/TickerBlock/Ticker.component.tsx
+++ src/features/StreetcodePage/TickerBlock/Ticker.component.tsx
@@ -5,34 +5,19 @@ import {useAsync} from "@hooks/stateful/useAsync.hook";
 import subtitlesApi from "@api/additional-content/subtitles.api";
 import Subtitle, {SubtitleStatus} from "@models/additional-content/subtitles.model";
 
-const tickerItems = [
-    "укладачі Інна Крупник, Оксана Юркова,",
-    "редакторка Ім`я Прізвище,",
-    "ілюстратор Сергій Федоров,","дизайнери Марія Кравцова, Олександр Івашко,",
-    "актор дубляжу Ім`я Прізвище,"
-];
-
 const TickerComponent = () => {
     const streetcodeId = useParams<{id: string}>();
     const id = parseInt(streetcodeId.id ?? "1");
     const {value} = useAsync(() =>subtitlesApi.getSubtitlesByStreetcodeId(id));
     let subtitles = value as Subtitle[];
-    console.log(subtitles);
 
     const createSubtitleString = (subtitles?: Subtitle[]): Map<number, string> => (
         new Map(subtitles?.map(({ id, firstName, lastName, subtitleStatus }) => [id, `${Object.keys(SubtitleStatus)[subtitleStatus]} ${firstName} ${lastName}, `] )) );
     const subtitleMap = createSubtitleString(subtitles);
-    console.log(subtitleMap);
-    const subtitless = Array.from(subtitleMap.values()).join('')
 
-    /*
-    subtitles = (subtitles || []).filter((element) => {
-        return element.streetcodeId===id;
-    });
-     */
     return (
         <Ticker className={"tickerContainer"}>
-            <div className={"tickerItem"}>{subtitless}</div>
+            <div className={"tickerItem"}>{Array.from(subtitleMap.values()).join('')}</div>
         </Ticker>
     );
 
