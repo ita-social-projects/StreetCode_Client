b10ba44149b86723925782e7132654cc9e3eda9d
EyR1oN <91558615+EyR1oN@users.noreply.github.com>
2023-01-12 14:08:29 +0200

    centered audio modal

--- src/features/StreetcodePage/MainBlock/MainBlock.component.tsx
+++ src/features/StreetcodePage/MainBlock/MainBlock.component.tsx
@@ -24,8 +24,8 @@ const MainBlock = () => {
                         <StreetcodeCard streetcode={streetcode} />
                     </div>
                 </div>
-                <ListenTextModal/>
             </div>
+            <ListenTextModal/>
         </div>
     );
 };
