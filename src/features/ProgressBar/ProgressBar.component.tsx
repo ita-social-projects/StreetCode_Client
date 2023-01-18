1e1c645e3e4593cbcac99b19e42f78c8364972bb
Alexander Lazarenko <ferrosful@gmail.com>
2023-01-18 15:26:25 +0200

    Improved component structure.

--- src/features/ProgressBar/ProgressBar.component.tsx
+++ src/features/ProgressBar/ProgressBar.component.tsx
@@ -3,13 +3,13 @@ import './ProgressBar.styles.scss';
 import ArrowUp from '@images/utils/ArrowUp.svg';
 
 import { useState } from 'react';
-import { MeasuredBlock } from '@features/ProgressBar/block-wrappers/StreetcodePageWrapper.component';
+import NavigableBlockWrapper, { MeasuredBlock } from '@features/ProgressBar/NavigableBlockWrapper.component';
 import useEventListener from '@hooks/external/useEventListener.hook';
 import useScrollPosition from '@hooks/scrolling/useScrollPosition/useScrollPosition.hook';
 import useToggle from '@hooks/stateful/useToggle.hook';
 
 interface Props {
-    blocks: MeasuredBlock[];
+    children: JSX.Element[];
     waitMsOnRender?: number;
 }
 
@@ -18,8 +18,9 @@ const getYScrollPercentage = (curPos: number, ofValue?: number) => {
     return (curPos / elHeight) * 100;
 };
 
-const ProgressBar = ({ waitMsOnRender = 50, blocks }: Props) => {
+const ProgressBar = ({ waitMsOnRender = 300, children }: Props) => {
     const [scrollPosition, setScrollPosition] = useState(0);
+    const [blocks, setBlocks] = useState<MeasuredBlock[]>([]);
     const { toggleState: isVisible, handlers: { toggle, off } } = useToggle();
 
     useScrollPosition(
@@ -32,47 +33,54 @@ const ProgressBar = ({ waitMsOnRender = 50, blocks }: Props) => {
     useEventListener('scroll', off, document);
 
     const totalHeight = blocks.reduce(
-        (acc: number, cur, idx) => acc
-            + ((idx === 0) ? cur.height * 2 : cur.height - blocks[idx - 1].height),
+        (acc: number, cur, idx) => acc + ((idx === 0) ? cur.height * 2 : cur.height - blocks[idx - 1].height),
         0,
     );
 
-    const scrollPercentage = getYScrollPercentage(scrollPosition, totalHeight);
-    const progressFillStyle = {
-        background: `linear-gradient(to bottom, #8D1F16
-            ${scrollPercentage * 0.8}%, transparent 0)`,
-    };
+    const scrollPercentage = getYScrollPercentage(scrollPosition, totalHeight)
+        - getYScrollPercentage(blocks[0]?.height, totalHeight);
+
+    console.log(totalHeight, scrollPosition, scrollPercentage, blocks.map((bl) => bl.height));
 
     return (
-        <div className="progressBarContainer" onClick={toggle}>
-            <div className={`progressBarPopupContainer ${isVisible ? 'visible' : ''}`}>
-                <div className="progressBarPopupContent">
-                    {blocks.map(({ id, height }, idx) => {
-                        let isBlockActive = height < scrollPosition;
-                        if (idx + 1 !== blocks.length) {
-                            isBlockActive &&= scrollPosition < blocks[idx + 1].height;
-                        }
+        <>
+            <NavigableBlockWrapper setBlocks={setBlocks}>
+                {children}
+            </NavigableBlockWrapper>
+            <div className="progressBarContainer" onClick={toggle}>
+                <div className={`progressBarPopupContainer ${isVisible ? 'visible' : ''}`}>
+                    <div className="progressBarPopupContent">
+                        {blocks.map(({ id, height }, idx) => {
+                            let isBlockActive = height < scrollPosition;
+                            if (idx + 1 !== blocks.length) {
+                                isBlockActive &&= scrollPosition < blocks[idx + 1].height;
+                            }
 
-                        return (
-                            <div key={id} className={`progressBarSection ${isBlockActive ? 'active' : ''}`}>
-                                <a href={`#${id}`}>
-                                    {idx + 1}
-                                </a>
-                            </div>
-                        );
-                    })}
-                    <span
-                        className="progressBarDashedFill"
-                        style={(blocks.length < 5) ? { height: '70%' } : undefined}
-                    />
-                    <span
-                        className="progressBarProgressFill"
-                        style={progressFillStyle}
-                    />
+                            // console.log(idx, height, scrollPosition, blocks[idx + 1]?.height, isBlockActive);
+
+                            return (
+                                <div key={id} className={`progressBarSection ${isBlockActive ? 'active' : ''}`}>
+                                    <a href={`#${id}`}>
+                                        {idx + 1}
+                                    </a>
+                                </div>
+                            );
+                        })}
+                        <span
+                            className="progressBarDashedFill"
+                            style={(blocks.length < 5) ? { height: '70%' } : undefined}
+                        />
+                        <span
+                            className="progressBarProgressFill"
+                            style={{ background: `linear-gradient(to bottom, #8D1F16
+                            ${scrollPercentage}%, transparent 0)` }}
+                        />
+                    </div>
                 </div>
+                <ArrowUp style={isVisible ? { rotate: 'x 180deg' } : undefined} />
+
             </div>
-            <ArrowUp style={isVisible ? { rotate: 'x 180deg' } : undefined} />
-        </div>
+        </>
     );
 };
 
--- src/features/StreetcodePage/Streetcode.component.tsx
+++ src/features/StreetcodePage/Streetcode.component.tsx
@@ -1,8 +1,5 @@
 import './Streetcode.styles.scss';
 
-import { useState } from 'react';
-import NavigableBlockWrapper, { MeasuredBlock }
-    from '@features/ProgressBar/block-wrappers/StreetcodePageWrapper.component';
 import ProgressBar from '@features/ProgressBar/ProgressBar.component';
 import Footer from '@layout/footer/Footer.component';
 import InterestingFactsComponent from '@streetcode/InterestingFactsBlock/InterestingFacts.component';
@@ -14,26 +11,21 @@ import SourcesComponent from '@streetcode/SourcesBlock/Sources.component';
 import TextComponent from '@streetcode/TextBlock/TextBlock.component';
 import TickerComponent from '@streetcode/TickerBlock/Ticker.component';
 
-const StreetcodeContent = () => {
-    const [blocks, setBlocks] = useState<MeasuredBlock[]>([]);
-
-    return (
-        <div className="streetcodeContainer">
-            <ProgressBar blocks={blocks} />
-            <MainBlock />
-            <NavigableBlockWrapper setBlocks={setBlocks}>
-                <TextComponent />
-                <InterestingFactsComponent />
-                <RelatedFiguresComponent />
-                <SourcesComponent />
-                <InterestingFactsComponent />
-            </NavigableBlockWrapper>
-            <QRComponent />
-            <PartnersComponent />
-            <TickerComponent />
-            <Footer />
-        </div>
-    );
-};
+const StreetcodeContent = () => (
+    <div className="streetcodeContainer">
+        <MainBlock />
+        <ProgressBar waitMsOnRender={50}>
+            <TextComponent />
+            <InterestingFactsComponent />
+            <RelatedFiguresComponent />
+            <SourcesComponent />
+            <InterestingFactsComponent />
+        </ProgressBar>
+        <QRComponent />
+        <PartnersComponent />
+        <TickerComponent />
+        <Footer />
+    </div>
+);
 
 export default StreetcodeContent;
