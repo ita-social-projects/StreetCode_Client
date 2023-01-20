89706bd529afc5a5ab628fa59dac21f115252f63
Alexander Lazarenko <ferrosful@gmail.com>
2023-01-20 14:44:04 +0200

    Added donate modal appearance on first page scroll.

--- src/features/StreetcodePage/DonateBtn/DonateBtn.component.tsx
+++ src/features/StreetcodePage/DonateBtn/DonateBtn.component.tsx
@@ -2,15 +2,30 @@ import './DonateBtn.styles.scss';
 
 import HandWithCoin from '@images/donates/HandWithCoin.svg';
 
+import { useRef } from 'react';
+import useScrollPosition from '@hooks/scrolling/useScrollPosition/useScrollPosition.hook';
 import useMobx from '@stores/root-store';
 
 const DonateBtn = () => {
+    const showModalOnScroll = useRef(true);
     const { modalStore: { setModal } } = useMobx();
+
+    useScrollPosition(({ currentPos: { y } }) => {
+        if (showModalOnScroll.current) {
+            if (Math.abs(y) > document.documentElement.scrollHeight * 0.845) {
+                setModal('donates', undefined, true);
+                showModalOnScroll.current = false;
+            }
+        }
+    });
 
     return (
         <div
             className="donateBtnContainer"
-            onClick={() => setModal('donates')}
+            onClick={() => {
+                setModal('donates');
+                showModalOnScroll.current = false;
+            }}
         >
             <div className="donateBtnCircle">
                 <HandWithCoin />
