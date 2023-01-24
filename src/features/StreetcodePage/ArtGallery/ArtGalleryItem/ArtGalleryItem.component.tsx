d25097f7a35b3d31444bbfcddcf291d2305e9ad5
Tatiana2424 <tanja3708@gmail.com>
2023-01-24 16:55:05 +0200

    fixed hover

--- src/features/StreetcodePage/ArtGallery/ArtGalleryItem/ArtGalleryItem.component.tsx
+++ src/features/StreetcodePage/ArtGallery/ArtGalleryItem/ArtGalleryItem.component.tsx
@@ -11,7 +11,7 @@ const ArtGalleryItem = ({ image, description, title }: Props) => {
     <div className="slideArt ">
       <div className={`img_wrap `}>
         <img className={"img_img"} src={image} alt="" />
-        <div className="img_data">
+        <div className={`img_data img_data-${description==undefined && title==undefined ? "empty" : "full"}`}>
           <p className="img_title">{title}</p>
           <p className="img_description">{description}</p>
         </div>
