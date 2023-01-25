77972b85d07c52d419ab20ea6771a97521876ee1
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-01-25 14:03:03 +0200

    Update ListenText.component.tsx

--- src/app/common/components/modals/ListenText/ListenText.component.tsx
+++ src/app/common/components/modals/ListenText/ListenText.component.tsx
@@ -3,24 +3,35 @@ import './ListenText.styles.scss';
 import ExitBtn from '@images/audio-player/ExitBtn.png';
 
 import { observer } from 'mobx-react-lite';
+import { useState } from 'react';
 import { AudioPlayer } from '@components/AudioPlayer/AudioPlayer.component';
 import useMobx from '@stores/root-store';
 
 const ListenTextModal = () => {
     const { modalStore } = useMobx();
     const { setModal, modalsState: { audio } } = modalStore;
+    const [closing, setClosing] = useState(false);
+
+    const handleClose = () => {
+        setClosing(true);
+        setTimeout(() => {
+            setModal('audio');
+            setClosing(false);
+        }, 1000);
+    };
 
     return (
         <>
-            {audio.isOpen
-            && (
-                <div className="modal">
+            {audio.isOpen && (
+                <div
+                    className={`modal ${closing ? 'fadeOutAnimation' : 'fadeInAnimation'}`}
+                >
                     <AudioPlayer />
                     <img
                         src={ExitBtn}
                         alt=""
                         className="closeModal"
-                        onClick={() => setModal('audio')}
+                        onClick={handleClose}
                     />
                 </div>
             )}
