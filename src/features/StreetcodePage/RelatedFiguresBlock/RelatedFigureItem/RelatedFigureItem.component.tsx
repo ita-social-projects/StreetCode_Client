5a59f4afca0101135ac0a3e9ab59c5aa7a837817
dimasster <dmytrobuchkovsky@gmail.com>
2023-01-28 23:28:47 +0200

    fix modal

--- src/features/StreetcodePage/RelatedFiguresBlock/RelatedFigureItem/RelatedFigureItem.component.tsx
+++ src/features/StreetcodePage/RelatedFiguresBlock/RelatedFigureItem/RelatedFigureItem.component.tsx
@@ -13,7 +13,7 @@ const redirectOnStreetcode = (id: number) => {
     console.log(`redirected to streetcode with id: ${id}`);
 };
 
-const RelatedFigureItem = ({ relatedFigure, filterTags = false }: Props) => {
+const RelatedFigureItem = ({ relatedFigure, filterTags = true }: Props) => {
     const { id, imageId, title, tags } = relatedFigure;
 
     const { imagesStore, tagsStore: { getTagArray } } = useMobx();
@@ -38,9 +38,9 @@ const RelatedFigureItem = ({ relatedFigure, filterTags = false }: Props) => {
                 </p>
                 <div className="relatedTagList">
                     {tags.filter((tag) => getTagArray.find((ti) => ti.id === tag.id || !filterTags))
-                        .map((tag, idx) => (
+                        .map((tag) => (
                         // eslint-disable-next-line react/no-array-index-key
-                            <div key={idx} className="tag">
+                            <div key={tag.id} className="tag">
                                 <p>{tag.title}</p>
                             </div>
                         ))}
