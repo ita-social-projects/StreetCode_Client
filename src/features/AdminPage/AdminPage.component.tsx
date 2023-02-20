704b6c9ac3a6845a752ecdfafc759e4a4a04d687
Slava <kasterov.v@gmail.com>
2023-02-20 16:31:33 +0200

    added base sctucture for admin-panel

---
+++ src/features/AdminPage/AdminPage.component.tsx
@@ -0,0 +1,9 @@
+import StreetcodesTable from "./StreetcodesTable/StreetcodesTable.component";
+
+const AdminPage = () => (
+    <div className="adminPageContainer">
+        <StreetcodesTable/>
+    </div>
+);
+
+export default AdminPage;
