4175426ddf78648282252234c5bf0acb5eeebb09
Katherine Derxen <92515141+Katerix@users.noreply.github.com>
2023-02-27 21:32:49 +0200

    additional changes

---
+++ src/features/AdminPage/NewStreetcode/MainNewStreetcode.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ForFansBlock from './ForFans/ForFans.component';
+import './MainNewStreetcode.styles.scss';
+
+const NewStreetcode = () => (
+    <ForFansBlock />
+);
+
+export default NewStreetcode;
