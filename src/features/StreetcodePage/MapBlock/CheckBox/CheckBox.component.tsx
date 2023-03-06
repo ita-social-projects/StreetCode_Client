3500841c418c81aa5a2389d626df017f60ff6f28
Vitalii <98163405+MementoMorj@users.noreply.github.com>
2023-03-06 12:27:40 +0200

    minor changes

--- src/features/StreetcodePage/MapBlock/CheckBox/CheckBox.component.tsx
+++ src/features/StreetcodePage/MapBlock/CheckBox/CheckBox.component.tsx
@@ -13,9 +13,9 @@ const CheckBoxComponent = () => {
         <div className="checkBoxContainer">
             <Radio.Group >
                 <Space direction="horizontal">
-                    <Checkbox className={"checkbox-text"} onChange={() => setCheckBox('streets')} checked={streets.isActive}>Вулиці</Checkbox >
-                    <Checkbox className={"checkbox-text"} onChange={() => setCheckBox('streetcodes')} checked={streetcodes.isActive}>Стріткоди</Checkbox >
-                    <Checkbox defaultChecked={false} disabled className={"checkbox-text"} onChange={() => setCheckBox('routes')} checked={routes.isActive}>Маршрути</Checkbox >
+                    <Checkbox className={"checkbox-line"} onChange={() => setCheckBox('streets')} checked={streets.isActive}> <h1 className="checkbox-text"> Вулиці </h1></Checkbox >
+                    <Checkbox className={"checkbox-line"} onChange={() => setCheckBox('streetcodes')} checked={streetcodes.isActive}><h1 className="checkbox-text"> Стріткоди </h1></Checkbox >
+                    {/* <Checkbox defaultChecked={false} disabled className={"checkbox-text"} onChange={() => setCheckBox('routes')} checked={routes.isActive}>Маршрути</Checkbox > */}
                     <InfoComponent/>
                 </Space>
             </Radio.Group>
