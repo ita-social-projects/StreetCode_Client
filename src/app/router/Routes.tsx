1004d941e04487344273bb73af5a6b455f552c13
Maksym Bratus <mbmaksbrat12345@gmail.com>
2023-03-15 18:36:23 +0200

    create not found page and private policy page

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -3,11 +3,15 @@ import FRONTEND_ROUTES from '@constants/frontend-routes.constants';
 import App from '@layout/app/App.component';
 import StreetcodeContent from '@streetcode/Streetcode.component';
 
+import NotFound from '@/features/AdditionalPages/NotFoundPage/NotFound.component';
+import PrivatePolicy from '@/features/AdditionalPages/PrivatePolicyPage/PrivatePolicy.component';
+
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path={FRONTEND_ROUTES.BASE} element={<App />}>
         <Route index path={`${FRONTEND_ROUTES.STREETCODE.BASE}/:id`} element={<StreetcodeContent />} />
         {/* <Route index path={FRONTEND_ROUTES.STREETCODE.BASE} element={<StreetcodeContent />} /> */}
-        {/* <Route path='*' element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
+        <Route path="/privacy-policy" element={<PrivatePolicy />} />
     </Route>,
 ));
 
