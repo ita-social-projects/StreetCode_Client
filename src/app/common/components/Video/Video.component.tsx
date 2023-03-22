fce02ce650c216463b0a32d55fbb2f146d97ab5d
Danyil Terentiev <dt210204@gmail.com>
2023-03-22 19:44:36 +0200

    Made an adaptive for textblock component

--- src/app/common/components/Video/Video.component.tsx
+++ src/app/common/components/Video/Video.component.tsx
@@ -1,22 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import './Video.styles.scss';
-
 import ReactPlayer from 'react-player';
 
 interface Props {
-  videoUrls: string[] | string;
+    videoUrls: string[] | string;
 }
 
-const VideoPlayer = ({ videoUrls, ...rest }: Props) => (
-    <div className="videoComponent">
-        <div {...rest}>
-            <ReactPlayer
-                className="react-player"
-                height="674px"
-                width="1200px"
-                url={videoUrls}
-                controls
-            />
+const VideoPlayer = ({ videoUrls, ...rest }: Props) => {
+    // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    //
+    // useEffect(() => {
+    //     function handleResize() {
+    //         setWindowWidth(window.innerWidth);
+    //     }
+    //
+    //     window.addEventListener('resize', handleResize);
+    //
+    //     return () => {
+    //         window.removeEventListener('resize', handleResize);
+    //     };
+    // }, []);
+    //
+    // let videoHeight = windowWidth > 768 ? 700 : 417; // Вычисляем высоту видео в зависимости от ширины окна
+    // if (windowWidth < 768 && windowWidth > 360) {
+    //     videoHeight = 417;
+    // } else if (windowWidth < 360) {
+    //     videoHeight = 190;
+    // }
+    return (
+        <div className="videoComponent">
+            <div className="player-wrapper">
+                <ReactPlayer
+                    width="100%"
+                    height="50vw"
+                    className="react-player"
+                    url={videoUrls}
+                    controls
+                />
+            </div>
         </div>
-    </div>
-);
+    );
+};
+
 export default VideoPlayer;
