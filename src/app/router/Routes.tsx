640ca8bd55038bec2ec27de47ec3a58f13f6187d
Kotusyk <melnikanna524@gmail.com>
2023-03-27 23:16:57 +0300

    Deleted not up-to-date components

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -3,16 +3,12 @@ import FRONTEND_ROUTES from '@constants/frontend-routes.constants';
 import App from '@layout/app/App.component';
 import StreetcodeContent from '@streetcode/Streetcode.component';
 import AdminPage from '@/features/AdminPage/AdminPage.component';
-import NewStreetcode from '@features/AdminPage/NewStreetcode/NewStreetcode.component';
-import ForFans from '@features/AdminPage/ForFans/ForFans.component';
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path={FRONTEND_ROUTES.BASE} element={<App />}>
         <Route index path={`${FRONTEND_ROUTES.STREETCODE.BASE}/:id`} element={<StreetcodeContent />} />
 
         <Route index path={`${FRONTEND_ROUTES.STREETCODE.BASE}/admin-panel`} element={<AdminPage />} />
-        <Route index path={`${FRONTEND_ROUTES.STREETCODE.BASE}/admin-panel/new-streetcode`} element={<NewStreetcode />} />
-        <Route index path={`${FRONTEND_ROUTES.STREETCODE.BASE}/admin-panel/for-fans`} element={<ForFans />} />
         {/* <Route index path={FRONTEND_ROUTES.STREETCODE.BASE} element={<StreetcodeContent />} /> */}
         {/* <Route path='*' element={<NotFound />} /> */}
     </Route>,
