b47c0886971d22566e9c77b0692bbb7c77c8039a
Slava <kasterov.v@gmail.com>
2023-03-28 13:06:42 +0300

    base64ToUrl added

--- src/models/media/image.model.ts
+++ src/models/media/image.model.ts
@@ -3,5 +3,7 @@ import Url from '@models/additional-content/url.model';
 export default interface Image {
     id: number;
     alt?: string | undefined;
-    url: Url;
+    blobName: string;
+    base64: string;
+    mimeType: string;
 }
