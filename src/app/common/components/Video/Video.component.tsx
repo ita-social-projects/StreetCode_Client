5099d08ea15b7358978bdcef2010cba4b4b5f6c8
Danyil Terentiev <dt210204@gmail.com>
2023-03-29 18:06:00 +0300

    Adapt video element

--- src/app/common/components/Video/Video.component.tsx
+++ src/app/common/components/Video/Video.component.tsx
@@ -1,18 +1,24 @@
-import React, { useState, useEffect } from 'react';
 import './Video.styles.scss';
+
 import ReactPlayer from 'react-player';
+import useWindowSize from '@hooks/stateful/useWindowSize.hook';
 
 interface Props {
     videoUrls: string[] | string;
 }
-
 const VideoPlayer = ({ videoUrls, ...rest }: Props) => {
+    const windowSize = useWindowSize();
+    let width = '50vw';
+
+    if (windowSize.width >= 1350) {
+        width = '45vw';
+    }
     return (
         <div className="videoComponent">
             <div className="player-wrapper">
                 <ReactPlayer
                     width="100%"
-                    height="50vw"
+                    height={width}
                     className="react-player"
                     url={videoUrls}
                     controls
