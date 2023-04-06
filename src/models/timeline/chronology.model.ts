a35fe2c959c782cec952148d7cc09f20f5a914cf
unknown <nadia.kischyk2003@gmail.com>
2023-04-06 22:21:00 +0300

    updateTimelineModel

--- src/models/timeline/chronology.model.ts
+++ src/models/timeline/chronology.model.ts
@@ -2,10 +2,18 @@ export interface HistoricalContext {
     id: number;
     title: string;
 }
+export enum DateViewType {
+    Year,
+    MonthYear,
+    SeasonYear,
+    DayMonthYear,
+}
 
 export default interface TimelineItem {
     id: number;
     date: Date;
+    dateString:string;
+    dateViewType:DateViewType,
     title: string;
     description?: string | undefined;
     historicalContexts: HistoricalContext[];
