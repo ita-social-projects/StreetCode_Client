6590580d11f27fb9d480bafa44cc3ead2099e602
Svitlana <103998860+Chynchenko@users.noreply.github.com>
2023-04-12 17:36:09 +0300

    sources adaptives

--- src/features/StreetcodePage/SourcesBlock/Sources.component.tsx
+++ src/features/StreetcodePage/SourcesBlock/Sources.component.tsx
@@ -1,45 +1,37 @@
 import './Sources.styles.scss';
-
 import { observer } from 'mobx-react-lite';
 import BlockSlider from '@features/SlickSlider/SlickSlider.component';
 import { useAsync } from '@hooks/stateful/useAsync.hook';
 import useMobx from '@stores/root-store';
 import BlockHeading from '@streetcode/HeadingBlock/BlockHeading.component';
-
 import SourceItem from './SourceItem/SourceItem.component';
+import useWindowSize from '@/app/common/hooks/stateful/useWindowSize.hook';
 
 const SourcesComponent = () => {
-    const { sourcesStore, streetcodeStore: { getStreetCodeId } } = useMobx();
-    const { fetchSrcCategoriesByStreetcodeId, getSrcCategoriesArray } = sourcesStore;
+  const { sourcesStore, streetcodeStore: { getStreetCodeId } } = useMobx();
+  const { fetchSrcCategoriesByStreetcodeId, getSrcCategoriesArray } = sourcesStore;
 
-    useAsync(
-        () => fetchSrcCategoriesByStreetcodeId(getStreetCodeId),
-        [getStreetCodeId],
-    );
+  const windowsize = useWindowSize();
+  const showDots = windowsize.width <= 1024;
 
-    return (
-        <div className="sourcesWrapper">
-            <div className="sourcesContainer">
-                <BlockHeading headingText="Для фанатів" />
-                <div className="sourceContentContainer">
-                    <div className="sourcesSliderContainer">
-                        <BlockSlider
-                            infinite={false}
-                            swipe={false}
-                            dots={false}
-                        >
-                            {getSrcCategoriesArray.flatMap((i) => [i, i]).map((sc) => (
-                                <SourceItem
-                                    key={sc.id}
-                                    srcCategory={sc}
-                                />
-                            ))}
-                        </BlockSlider>
-                    </div>
-                </div>
-            </div>
+  useAsync(() => fetchSrcCategoriesByStreetcodeId(getStreetCodeId), [getStreetCodeId]);
+
+  return (
+    <div className="sourcesWrapper">
+      <div className="sourcesContainer">
+        <BlockHeading headingText="Для фанатів" />
+        <div className="sourceContentContainer">
+          <div className="sourcesSliderContainer">
+          <BlockSlider infinite={false} swipe={false} dots={showDots} initialSlide={0}>
+  {getSrcCategoriesArray.flatMap((i) => [i, i]).map((sc) => (
+    <SourceItem key={sc.id} srcCategory={sc} />
+  ))}
+</BlockSlider>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default observer(SourcesComponent);
