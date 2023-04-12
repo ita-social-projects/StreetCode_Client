4cd68c26ba586794da52b8cf687c17f9ddf4c340
Kuzmych Roman <67451349+Dobriyr@users.noreply.github.com>
2023-04-12 19:06:38 +0300

    update subtitle

--- src/models/additional-content/subtitles.model.ts
+++ src/models/additional-content/subtitles.model.ts
@@ -2,17 +2,8 @@ import Streetcode from '@models/streetcode/streetcode-types.model';
 
 import Url from './url.model';
 
-export enum SubtitleStatus {
-    Editor = 'Editor',
-    Illustrator = 'Illustrator',
-}
-
 export default interface Subtitle {
     id: number;
-    subtitleStatus: number;
-    firstName: string;
-    lastName: string;
-    description?: string | undefined;
-    url: Url;
-    streetcode: Streetcode;
+    subtitleText: string;
+    streetcodeId: number;
 }
