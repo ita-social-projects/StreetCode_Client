7c8526170d3cb674d074b488d64bcdf0c2ff56a9
Kateryna Derxen <92515141+Katerix@users.noreply.github.com>
2023-04-13 11:10:09 +0300

    updated skeketon & heading done

--- src/features/AdditionalPages/SupportUsPage/SupportUs.component.tsx
+++ src/features/AdditionalPages/SupportUsPage/SupportUs.component.tsx
@@ -1,7 +1,19 @@
 import './SupportUs.styles.scss';
+import '../ContactUsPage/Title/Title.styles.scss';
 
 const SupportUs = () => {
-    return (<></>)
+    return (
+        <div className='supportUsPage'>
+            <div className="titleBig"> Підтримати нас</div>
+            <div className="titleSmall heading">
+                Привіт! Після всіх важливих сторінок нашої платформи ти нарешті тут — на не менш важливій сторінці твоєї залученості та підтримки.
+            </div>
+            <div className='text heading'>
+                Наша команда горить-палає місією «застріткодити всю Україну», розповісти про видатних Героїв та подати цікавенні факти з історії. 
+                Так, це ми. Але окрім внутрішніх волонтерських вогників наших сердець проєкту для стійкості та розвитку...
+            </div>
+        </div>
+    )
 }
 
 export default SupportUs;
