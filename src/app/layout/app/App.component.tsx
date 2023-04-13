8b506113b1ed5e912ef83f84bec24a3f9297d7c2
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-04-13 15:26:00 +0300

    Initial
    Created testing connection with GA4

--- src/app/layout/app/App.component.tsx
+++ src/app/layout/app/App.component.tsx
@@ -2,12 +2,15 @@ import './App.styles.scss';
 import './ant-styles.overrides.scss';
 
 import { observer } from 'mobx-react-lite';
+import ReactGA from 'react-ga4';
 import { Outlet, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import FRONTEND_ROUTES from '@constants/frontend-routes.constants';
 import HeaderBlock from '@layout/header/HeaderBlock.component';
 import ModalWrapper from '@layout/ModalWrapper.component';
 import useMobx from '@stores/root-store';
+
+// ReactGA.initialize('your GA measurement id');
 
 const App = () => {
     const { pathname } = useLocation();
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import '@fonts/fonts.styles.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga';
 import { RouterProvider } from 'react-router-dom';
 import router from '@app/router/Routes';
+
+ReactGA.initialize('Your Unique ID');
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
