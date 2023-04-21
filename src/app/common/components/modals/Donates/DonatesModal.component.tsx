921babf4da176335cb6ef10c0d464ef7ebb9897f
Lolimkeri <demobor96@gmail.com>
2023-04-21 14:05:06 +0300

    Fixed donates modal for Pc and mobile

--- src/app/common/components/modals/Donates/DonatesModal.component.tsx
+++ src/app/common/components/modals/Donates/DonatesModal.component.tsx
@@ -12,7 +12,7 @@ import { observer } from 'mobx-react-lite';
 import useMobx from '@stores/root-store';
 import axios from 'axios';;
 
-const possibleDonateAmounts = [100, 500, 1000];
+const possibleDonateAmounts = [500, 100, 50];
 
 const DonatesModal = () => {
     const { modalStore } = useMobx();
@@ -41,32 +41,49 @@ const DonatesModal = () => {
 
     const handleDonateInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
         let newValue = target.value.replace('₴', '').trim();
-        
         if (!newValue) {
-          setDonateAmount(0);
+            setDonateAmount(0);
         } else {
-          const parsedValue = parseInt(newValue, 10);
-          if (Number.isSafeInteger(parsedValue)) {
-            setDonateAmount(parsedValue);
-          } else {
-            setDonateAmount(donateAmount);
-          }
+            const parsedValue = parseInt(newValue, 10);
+            if (Number.isSafeInteger(parsedValue)) {
+                setDonateAmount(parsedValue);
+            } else {
+                setDonateAmount(donateAmount);
+            }
         }
-      };
-      
+    };
+    
+    const minWidth = 0; // Minimum width of the input field
+    const charWidth = 42; // Width of each character in pixels
+    const firstWidth = 13;
+    const mobCharWidth = 21;
+    const mobFirstWidth = 6;
+    const count = (donateAmount.toString().match(/1/g) || []).length;
+    
+    var inputWidth = 0;
+    if(window.innerWidth>1024)
+    {
+        inputWidth = minWidth + donateAmount.toString().length * charWidth - count*firstWidth;
+    }
+    else
+    {
+        inputWidth = minWidth + donateAmount.toString().length * mobCharWidth - count*mobFirstWidth;
+    }
+    const style = { "--input-width": `${inputWidth}px` } as React.CSSProperties;
+
     const handlePost = async () => {
         if (isCheckboxChecked) {
-          try {
-            const response = await axios.post(`${linkBase}?${donateAmount}&${donateComment}`);
-            window.location.replace(response.data);
-          } catch (err) {
-            console.error(err);
-          }
+            try {
+                const response = await axios.post(`${linkBase}?${donateAmount}&${donateComment}`);
+                window.location.replace(response.data);
+            } catch (err) {
+                console.error(err);
+            }
         } else {
-          console.log('Checkbox not checked');
+            console.log('Checkbox not checked');
         }
-      }
-      
+    }
+
 
     useEffect(() => {
         const handleResize = () => {
@@ -104,12 +121,16 @@ const DonatesModal = () => {
                 <h1>Підтримай проєкт</h1>
                 <h3>Скажи «Дякую» історії</h3>
                 <div className="enterSum">Ввести суму</div>
-                <input
-                    onChange={handleDonateInputChange}
-                    style={inputStyle}
-                    value={`${donateAmount.toString()}₴`}
-                    className={`amountInput ${(donateAmount !== 0) ? 'active' : ''}`}
-                />
+                <div className="donateInputContainer">
+                    <input
+                        onChange={handleDonateInputChange}
+                        style={{ ...style, width: `var(--input-width)` }}
+                        maxLength={14}
+                        value={`${donateAmount.toString()}`}
+                        className={`amountInput ${(donateAmount !== 0) ? 'active' : ''} `}
+                    />
+                    <div className={`amountInput ${(donateAmount !== 0) ? 'active' : ''} GryvnaSymbol`}>₴</div>
+                </div>
                 <div className="donatesBtnContainer">
                     {possibleDonateAmounts.map((amount, idx) => (
                         <Button
@@ -124,11 +145,12 @@ const DonatesModal = () => {
                     ))}
                 </div>
                 <div className="donatesInputContainer">
-                        <Checkbox className={"checkbox-borderline"}  checked={isCheckboxChecked} onChange={(e) => setIsCheckboxChecked(e.target.checked)}>Я даю згоду на обробку моїх персональних даних</Checkbox>
+                    <Checkbox className={"checkbox-borderline"} checked={isCheckboxChecked} onChange={(e) => setIsCheckboxChecked(e.target.checked)}>Я даю згоду на обробку моїх персональних даних</Checkbox>
                 </div>
-                <Button onClick={handlePost} 
+                <Button onClick={handlePost}
                     disabled={!isCheckboxChecked}
-                    className="donatesDonateBtn">Підтримати</Button>
+                    className="donatesDonateBtn"
+                >Підтримати</Button>
             </div>
         </Modal>
     );
