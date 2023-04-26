a51fb65e38f158fde2d91c5de1c7eaef6b1929d8
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-04-26 20:03:59 +0300

    Redirect page init

---
+++ src/features/AdditionalPages/RedirectPage/RedirectPage.component.tsx
@@ -0,0 +1,17 @@
+import './RedirectPage.styles.scss';
+
+import { useEffect } from 'react';
+
+const RedirectPage = () => {
+    useEffect(() => {
+
+    });
+
+    return (
+        <div>
+            <p>Redirect...</p>
+        </div>
+    );
+};
+
+export default RedirectPage;
