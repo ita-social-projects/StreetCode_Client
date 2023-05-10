7c79e14eab5ad182f8f0bde72592901c95a3e055
ValDekh <smithdekh@gmail.com>
2023-05-10 15:48:02 +0300

    LastOne

--- src/features/AdminPage/NewStreetcode/TextBlock/TextForm/AdditionTextBlock/AdditionalTextBlockAdminForm.component.tsx
+++ src/features/AdminPage/NewStreetcode/TextBlock/TextForm/AdditionTextBlock/AdditionalTextBlockAdminForm.component.tsx
@@ -7,6 +7,7 @@ import { observer } from 'mobx-react-lite';
 import { Editor } from '@tinymce/tinymce-react';
 
 import FormItem from 'antd/es/form/FormItem';
+
 import TextInputInfo from '../../InputType/TextInputInfo.model';
 
 interface Props {
@@ -15,34 +16,32 @@ interface Props {
 }
 
 const AdditionalTextBlockAdminForm = ({ inputInfo, setInputInfo }:
-    Props) => {
-    return (
-        <FormItem label="Авторство">
-            <Editor
-                init={{
-                    height: 300,
-                    menubar: false,
-                    init_instance_callback(editor) {
-                        editor.setContent(inputInfo.additionalText);
-                    },
-                    plugins: [
-                        'autolink',
-                        'lists', 'preview', 'anchor', 'searchreplace', 'visualblocks',
-                        'insertdatetime', 'wordcount', 'link', 'lists', 'formatselect ',
-                    ],
-                    toolbar: 'undo redo blocks bold italic link align | underline superscript subscript '
+    Props) => (
+    <FormItem label="Авторство">
+        <Editor
+            init={{
+                height: 300,
+                menubar: false,
+                init_instance_callback(editor) {
+                    editor.setContent(inputInfo.additionalText);
+                },
+                plugins: [
+                    'autolink',
+                    'lists', 'preview', 'anchor', 'searchreplace', 'visualblocks',
+                    'insertdatetime', 'wordcount', 'link', 'lists', 'formatselect ',
+                ],
+                toolbar: 'undo redo blocks bold italic link align | underline superscript subscript '
                         + 'formats blockformats align | removeformat strikethrough ',
-                    content_style: 'body {font - family:Roboto,Helvetica Neue,sans-serif; font-size:14px }',
-                    placeholder: 'Текст підготовлений спільно з...',
-                }}
+                content_style: 'body {font - family:Roboto,Helvetica Neue,sans-serif; font-size:14px }',
+                placeholder: 'Текст підготовлений спільно з...',
+            }}
 
-                onChange={(e, editor) => {
-                    setInputInfo({ ...inputInfo, additionalText: editor.getContent() });
-                    console.log('input: ', inputInfo);
-                }}
-            />
-        </FormItem>
-    );
-};
+            onChange={(e, editor) => {
+                setInputInfo({ ...inputInfo, additionalText: editor.getContent() });
+                console.log('input: ', inputInfo);
+            }}
+        />
+    </FormItem>
+);
 
 export default observer(AdditionalTextBlockAdminForm);
