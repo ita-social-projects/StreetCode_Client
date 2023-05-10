8d3c92bf013f424fcee8b7111ed9b107df593a71
Vitalii <98163405+MementoMorj@users.noreply.github.com>
2023-05-10 13:32:23 +0300

    1

--- src/features/MainPage/MainPage.component.tsx
+++ src/features/MainPage/MainPage.component.tsx
@@ -4,23 +4,20 @@ import Footer from "../../app/layout/footer/Footer.component";
 import ProgressBar from "../ProgressBar/ProgressBar.component";
 import DonateBtn from "../StreetcodePage/DonateBtn/DonateBtn.component";
 import TextBlockComponent from '../StreetcodePage/TextBlock/TextBlock.component';
-
+import TopCarouselComponent from './TopCarousel/TopCarousel.component';
 const mainPageContent = () => {
     return (
         <div className="mainPageContainer">
-
-            {/*<ProgressBar >*/}
-
-            {/*</ProgressBar>*/}
             <div className="sticky">
                 <div className="sticky-content">
                     <ScrollToTopBtn />
                     <DonateBtn />
                 </div>
             </div>
-            <Footer />
+            <TopCarouselComponent/>
+            {/* <Footer /> */}
         </div>
     );
-
 }
+
 export default mainPageContent;
