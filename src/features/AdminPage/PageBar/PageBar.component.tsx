943187a6494e8e012eff8fd74d3ced3775465e36
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-05-15 14:52:38 +0300

    Update PageBar.component.tsx

--- src/features/AdminPage/PageBar/PageBar.component.tsx
+++ src/features/AdminPage/PageBar/PageBar.component.tsx
@@ -11,7 +11,7 @@ const PageBar = () => (
             <Link className='Link' to="#">Користувач</Link>
             <Link className='Link' to={FRONTEND_ROUTES.ADMIN.PARTNERS}>Партнери</Link>
             <Link className='Link' to={FRONTEND_ROUTES.ADMIN.FOR_FANS}>Для фанатів</Link>
-            <Link className='Link' to={`${FRONTEND_ROUTES.STREETCODE.BASE}/admin-panel/for-fans`}>Для фанатів</Link>
+            <Link className='Link' to={`${FRONTEND_ROUTES.ADMIN.FOR_FANS}`}>Для фанатів</Link>
             <Link className='Link' to={`${FRONTEND_ROUTES.ADMIN.TEAM}`}>Команда</Link>
         </div>
     </div>
