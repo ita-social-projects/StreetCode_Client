b762d3788c4085fe3299714cb3088f30042e2b36
unknown <nadia.kischyk2003@gmail.com>
2023-05-16 13:40:31 +0300

    fix partners on the modal checkbox and the selection in new streetcode

--- src/features/AdminPage/PartnersPage/PartnerModal/PartnerModal.component.tsx
+++ src/features/AdminPage/PartnersPage/PartnerModal/PartnerModal.component.tsx
@@ -1,6 +1,8 @@
 import './PartnerModal.styles.scss';
 import '@features/AdminPage/AdminModal.styles.scss';
 
+import CancelBtn from '@images/utils/Cancel_btn.svg';
+
 import { observer } from 'mobx-react-lite';
 import React, { useEffect, useRef, useState } from 'react';
 import { DeleteOutlined, UserAddOutlined } from '@ant-design/icons';
@@ -14,9 +16,10 @@ import {
 } from 'antd';
 import FormItem from 'antd/es/form/FormItem';
 import TextArea from 'antd/es/input/TextArea';
-import CancelBtn from '@images/utils/Cancel_btn.svg';
+import { CheckboxProps } from 'antd/lib/checkbox';
 
 import ImagesApi from '@/app/api/media/images.api';
+import partnersApi from '@/app/api/partners/partners.api';
 import FileUploader from '@/app/common/components/FileUploader/FileUploader.component';
 import base64ToUrl from '@/app/common/utils/base64ToUrl.utility';
 import PartnerLink from '@/features/AdminPage/PartnersPage/PartnerLink.component';
@@ -28,7 +31,7 @@ import Partner, {
 import { StreetcodeShort } from '@/models/streetcode/streetcode-types.model';
 
 const PartnerModal:React.FC<{ partnerItem?:Partner, open:boolean, isStreetcodeVisible?:boolean,
-    setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>, afterSubmit?:(partner:PartnerCreateUpdate)=>void
+    setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>, afterSubmit?:(partner:Partner)=>void
  }> = observer(({
      partnerItem, open, setIsModalOpen, isStreetcodeVisible = true, afterSubmit,
  }) => {
@@ -122,7 +125,7 @@ const PartnerModal:React.FC<{ partnerItem?:Partner, open:boolean, isStreetcodeVi
          });
          const partner: PartnerCreateUpdate = {
              id: 0,
-             isKeyPartner: formValues.isKeyPartner,
+             isKeyPartner: formValues.isKeyPartner ?? false,
              logoId: imageId.current,
              partnerSourceLinks,
              streetcodes: selectedStreetcodes.current,
@@ -130,35 +133,37 @@ const PartnerModal:React.FC<{ partnerItem?:Partner, open:boolean, isStreetcodeVi
              title: formValues.title,
              description: formValues.description ?? '',
              urlTitle: formValues.urlTitle ?? '',
-             isVisibleEverywhere: false,
+             isVisibleEverywhere: formValues.isVisibleEverywhere ?? false,
          };
-         let success = false;
          if (partnerItem) {
              partner.id = partnerItem.id;
              Promise.all([
                  partnersStore.updatePartner(partner)
-                     .then(() => {
-                         success = true;
+                     .then((p) => {
+                         if (afterSubmit) {
+                             console.log('fater');
+                             if (p) {
+                                 console.log('if');
+                                 afterSubmit({ ...p });
+                             }
+                         }
                      })
                      .catch((e) => {
-                         success = false;
                      }),
              ]);
          } else {
              Promise.all([
-                 partnersStore.createPartner(partner)
-                     .then(() => {
-                         success = true;
+                 await partnersApi.create(partner)
+                     .then((p) => {
+                         if (afterSubmit) {
+                             afterSubmit(p);
+                         }
                      })
                      .catch((e) => {
-                         success = false;
                      }),
              ]);
          }
          closeAndCleanData();
-         if (success && afterSubmit) {
-             afterSubmit(partner);
-         }
      };
 
      const selectSocialMediaOptions = [{
@@ -178,120 +183,122 @@ const PartnerModal:React.FC<{ partnerItem?:Partner, open:boolean, isStreetcodeVi
          <Modal
              open={open}
              onCancel={closeAndCleanData}
-             className="modalContainer"
-             footer = {null}
+             className="partner-modal-container"
+             footer={null}
              closeIcon={<CancelBtn />}
          >
-            <div className='modalContainer-content'>
-             <Form
-                 form={form}
-                 layout='vertical'
-                 onFinish={onSuccesfulSubmitPartner}
-             >
-                <div className='center'>
-                    <h2>{partnerItem ? 'Редагувати' : 'Додати'} партнера</h2>
-                </div>
-                <div className="checkbox-container">
-                     <Form.Item
-                         name="isKeyPartner"
-                         valuePropName="checked"
-                     >
-                        <div className="checkbox-container-flex">
-                            <Checkbox />
-                            <p>Ключовий партнер</p>
-                        </div>
-                     </Form.Item>
-
-                     <Form.Item
-                         name="isVisibleEverywhere"
-                         valuePropName="checked"
-                     >
-                        <div className="checkbox-container-flex">
-                            <Checkbox />
-                            <p>Видимий для всіх</p>
-                        </div>
-                     </Form.Item>
-                 </div>
-
-                 <Form.Item
-                     name="title"
-                     label="Назва: "
-                     rules={[{ required: true, message: 'Введіть назву' }]}
+             <div className="modalContainer-content">
+                 <Form
+                     form={form}
+                     layout="vertical"
+                     onFinish={onSuccesfulSubmitPartner}
                  >
-                     <Input maxLength={100} showCount />
-                 </Form.Item>
-
-                 <Form.Item
-                     name="url"
-                     label="Посилання: "
-                 >
-                     <Input maxLength={200} showCount />
-                 </Form.Item>
-
-                 <Form.Item
-                     name="urlTitle"
-                     label="Назва посилання: "
-                 >
-                     <Input maxLength={100} showCount />
-                 </Form.Item>
-
-                 <Form.Item
-                     name="description"
-                     label="Опис: "
-                 >
-                     <TextArea showCount maxLength={450} />
-                 </Form.Item>
-
-                 <Form.Item
-                     name="logo"
-                     label="Лого"
-                     valuePropName="fileList"
-                     getValueFromEvent={(e: any) => {
-                         if (Array.isArray(e)) {
-                             return e;
-                         }
-                         return e?.fileList;
-                     }}
-                 >
-                     <FileUploader
-                         multiple={false}
-                         accept=".jpeg,.png,.jpg"
-                         listType="picture-card"
-                         maxCount={1}
-                         onPreview={handlePreview}
-                         uploadTo="image"
-                         onSuccessUpload={(image:Image) => {
-                             imageId.current = image.id;
-                         }}
-                         onRemove={(image) => {
-                             ImagesApi.delete(imageId.current);
-                         }}
-                         defaultFileList={(partnerItem)
-                             ? [{ name: '',
-                                  thumbUrl: base64ToUrl(partnerItem.logo?.base64, partnerItem.logo?.mimeType),
-                                  uid: partnerItem.logoId.toString(),
-                                  status: 'done' }]
-                             : []}
-                     >
-                         <p>Виберіть чи перетягніть файл</p>
-                     </FileUploader>
-                 </Form.Item>
-                 <PreviewFileModal opened={previewOpen} setOpened={setPreviewOpen} file={filePreview} />
-
-                 {isStreetcodeVisible ? (
-                     <Form.Item name="partnersStreetcodes" label="Стріткоди: ">
-                         <Select
-                             mode="multiple"
-                             onSelect={onStreetcodeSelect}
-                             onDeselect={onStreetcodeDeselect}
+                     <div className="center">
+                         <h2>
+                             {partnerItem ? 'Редагувати' : 'Додати'}
+                             {' '}
+партнера
+                         </h2>
+                     </div>
+                     <div className="checkbox-container">
+                         <Form.Item
+                             className="line-form-item"
+                             name="isKeyPartner"
+                             valuePropName="checked"
+                             label="Ключовий партнер: "
                          >
-                             {streetcodeShortStore.streetcodes
-                                 .map((s) => <Select.Option key={`${s.id}`} value={s.title}>{s.title}</Select.Option>)}
-                         </Select>
+                             <Checkbox />
+                         </Form.Item>
+
+                         <Form.Item
+                             className="line-form-item"
+                             name="isVisibleEverywhere"
+                             valuePropName="checked"
+                             label="Видимий для всіх: "
+                         >
+                             <Checkbox />
+                         </Form.Item>
+                     </div>
+
+                     <Form.Item
+                         name="title"
+                         label="Назва: "
+                         rules={[{ required: true, message: 'Введіть назву' }]}
+                     >
+                         <Input maxLength={100} showCount />
                      </Form.Item>
-                 ) : ''}
-                
-             </Form>
+
+                     <Form.Item
+                         name="url"
+                         label="Посилання: "
+                     >
+                         <Input maxLength={200} showCount />
+                     </Form.Item>
+
+                     <Form.Item
+                         name="urlTitle"
+                         label="Назва посилання: "
+                     >
+                         <Input maxLength={100} showCount />
+                     </Form.Item>
+
+                     <Form.Item
+                         name="description"
+                         label="Опис: "
+                     >
+                         <TextArea showCount maxLength={450} />
+                     </Form.Item>
+
+                     <Form.Item
+                         name="logo"
+                         label="Лого"
+                         valuePropName="fileList"
+                         getValueFromEvent={(e: any) => {
+                             if (Array.isArray(e)) {
+                                 return e;
+                             }
+                             return e?.fileList;
+                         }}
+                     >
+                         <FileUploader
+                             multiple={false}
+                             accept=".jpeg,.png,.jpg"
+                             listType="picture-card"
+                             maxCount={1}
+                             onPreview={handlePreview}
+                             uploadTo="image"
+                             onSuccessUpload={(image:Image) => {
+                                 imageId.current = image.id;
+                             }}
+                             onRemove={(image) => {
+                                 ImagesApi.delete(imageId.current);
+                             }}
+                             defaultFileList={(partnerItem)
+                                 ? [{ name: '',
+                                      thumbUrl: base64ToUrl(partnerItem.logo?.base64, partnerItem.logo?.mimeType),
+                                      uid: partnerItem.logoId.toString(),
+                                      status: 'done' }]
+                                 : []}
+                         >
+                             <p>Виберіть чи перетягніть файл</p>
+                         </FileUploader>
+                     </Form.Item>
+                     <PreviewFileModal opened={previewOpen} setOpened={setPreviewOpen} file={filePreview} />
+
+                     {isStreetcodeVisible ? (
+                         <Form.Item name="partnersStreetcodes" label="Стріткоди: ">
+                             <Select
+                                 mode="multiple"
+                                 onSelect={onStreetcodeSelect}
+                                 onDeselect={onStreetcodeDeselect}
+                             >
+                                 {streetcodeShortStore.streetcodes
+                                     .map((s) => <Select.Option key={`${s.id}`} value={s.title}>{s.title}</Select.Option>)}
+                             </Select>
+                         </Form.Item>
+                     ) : ''}
+
+                 </Form>
              </div>
 
              <Form
@@ -304,6 +311,7 @@ const PartnerModal:React.FC<{ partnerItem?:Partner, open:boolean, isStreetcodeVi
                      {partnerSourceLinks.map((link) => (
                          <div
                              key={`${link.id}${link.logoType}`}
+                             className="partner-source-list-item"
                          >
                              <PartnerLink link={link} />
                              <p>{link.targetUrl}</p>
@@ -325,7 +333,7 @@ const PartnerModal:React.FC<{ partnerItem?:Partner, open:boolean, isStreetcodeVi
                          />
                      </FormItem>
                      <Form.Item
-                         label=" "
+                         label="Посилання"
                          className="url-input"
                          name="url"
                          rules={[{ required: true, message: 'Введіть посилання' }]}
@@ -333,19 +341,20 @@ const PartnerModal:React.FC<{ partnerItem?:Partner, open:boolean, isStreetcodeVi
                          <Input min={1} max={255} showCount />
                      </Form.Item>
 
-                     <Form.Item 
-                        label=" ">
+                     <Form.Item
+                         label=" "
+                     >
                          <Button htmlType="submit">
                              <UserAddOutlined />
                          </Button>
                      </Form.Item>
                  </div>
                  {customWarningVisible ? <p className="red-text">Посилання не співпадає з вибраним текстом</p> : ''}
-                 
-                 <div className='center'>
-                    <Button className="streetcode-custom-button" onClick={() => form.submit()}>
+
+                 <div className="center">
+                     <Button className="streetcode-custom-button" onClick={() => form.submit()}>
                             Зберегти
-                    </Button>
+                     </Button>
                  </div>
              </Form>
          </Modal>
