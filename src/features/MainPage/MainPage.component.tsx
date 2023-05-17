3b193ef95931ec4c4b91bc8df9d48a80178fc6a1
Maksym Bratus <mbmaksbrat12345@gmail.com>
2023-05-17 21:26:24 +0300

    add partner block to main page

--- src/features/MainPage/MainPage.component.tsx
+++ src/features/MainPage/MainPage.component.tsx
@@ -1,25 +1,28 @@
 import './MainPage.styles.scss';
-import ScrollToTopBtn from "../../app/common/components/ScrollToTopBtn/ScrollToTopBtn.component";
-import Footer from "../../app/layout/footer/Footer.component";
-import DonateBtn from "../StreetcodePage/DonateBtn/DonateBtn.component";
-import TopCarouselComponent from './TopCarousel/TopCarousel.component';
+
+import ScrollToTopBtn from '../../app/common/components/ScrollToTopBtn/ScrollToTopBtn.component';
+import Footer from '../../app/layout/footer/Footer.component';
+import DonateBtn from '../StreetcodePage/DonateBtn/DonateBtn.component';
+
+import PartnersBlockComponent from './PartnersBlockMain/PartnersBlockMain.component';
 import StreetcodeSliderComponent from './StreetcodeSlider/StreetcodeSlider.component';
 import TeamComponent from './TeamSlider/TeamComponent.component';
-const mainPageContent = () => {
-    return (
-        <div className="mainPageContainer">
-            <div className="sticky">
-                <div className="sticky-content">
-                    <ScrollToTopBtn />
-                    <DonateBtn />
-                </div>
+import TopCarouselComponent from './TopCarousel/TopCarousel.component';
+
+const mainPageContent = () => (
+    <div className="mainPageContainer">
+        <div className="sticky">
+            <div className="sticky-content">
+                <ScrollToTopBtn />
+                <DonateBtn />
             </div>
-            <TopCarouselComponent/>
-            <StreetcodeSliderComponent />
-            <TeamComponent />
-            <Footer />
         </div>
-    );
-}
+        <TopCarouselComponent />
+        <StreetcodeSliderComponent />
+        <TeamComponent />
+        <PartnersBlockComponent />
+        <Footer />
+    </div>
+);
 
 export default mainPageContent;
---
+++ src/features/MainPage/PartnersBlockMain/PartnersBlockMain.component.tsx
@@ -0,0 +1,11 @@
+import './PartnersBlockMain.styles.scss';
+
+import PartnersBlock from '@/features/AdditionalPages/PartnersPage/PartnersBlock/PartnersBlock.component';
+
+const PartnersBlockComponent = () => (
+    <div className="partnersBlockMainContainer">
+        <PartnersBlock />
+    </div>
+);
+
+export default PartnersBlockComponent;
