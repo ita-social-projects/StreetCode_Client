d53d21db8032e0c1e0e2a382bfccf46c5632903a
Danyil Terentiev <dt210204@gmail.com>
2023-05-22 20:37:30 +0300

    Added field for creation date in modal and display on a table

--- src/features/AdminPage/NewsPage/NewsModal/NewsModal.component.tsx
+++ src/features/AdminPage/NewsPage/NewsModal/NewsModal.component.tsx
@@ -5,10 +5,13 @@ import React, { useEffect, useRef, useState } from 'react';
 import PreviewFileModal from '@features/AdminPage/NewStreetcode/MainBlock/PreviewFileModal/PreviewFileModal.component';
 import useMobx from '@stores/root-store';
 import { Editor as TinyMCEEditor } from '@tinymce/tinymce-react/lib/cjs/main/ts/components/Editor';
+import dayjs from 'dayjs';
 
 import {
-    Button, Form, Input, Modal, UploadFile,
+    Button, ConfigProvider, DatePicker, Form, Input, Modal, UploadFile,
 } from 'antd';
+import ukUAlocaleDatePicker from 'antd/es/date-picker/locale/uk_UA';
+import ukUA from 'antd/locale/uk_UA';
 
 import FileUploader from '@/app/common/components/FileUploader/FileUploader.component';
 import base64ToUrl from '@/app/common/utils/base64ToUrl.utility';
@@ -52,7 +55,10 @@ const NewsModal:React.FC<{ newsItem?:News, open:boolean,
          form.resetFields();
          setIsModalOpen(false);
      };
-
+     dayjs.locale('uk');
+     const dayJsUa = require("dayjs/locale/uk"); // eslint-disable-line
+     ukUAlocaleDatePicker.lang.shortWeekDays = dayJsUa.weekdaysShort;
+     ukUAlocaleDatePicker.lang.shortMonths = dayJsUa.monthsShort;
      const onSuccessfulSubmitNews = async (formValues:any) => {
          const news: News = {
              id: 0,
@@ -60,6 +66,7 @@ const NewsModal:React.FC<{ newsItem?:News, open:boolean,
              url: formValues.url,
              title: formValues.title,
              text: editorRef.current.getContent(),
+             creationDate: formValues.creationDate,
          };
 
          let success = false;
@@ -94,105 +101,120 @@ const NewsModal:React.FC<{ newsItem?:News, open:boolean,
      };
 
      return (
-         <Modal
-             open={open}
-             onCancel={closeAndCleanData}
-             className="modalContainer"
-             closeIcon={<CancelBtn />}
-             footer={null}
-         >
-             <div className="modalContainer-content">
-                 <Form
-                     form={form}
-                     layout="vertical"
-                     onFinish={onSuccessfulSubmitNews}
+         <div>
+             <ConfigProvider locale={ukUA}>
+                 <Modal
+                     open={open}
+                     onCancel={closeAndCleanData}
+                     className="modalContainer"
+                     closeIcon={<CancelBtn />}
+                     footer={null}
                  >
-                     <div className="center">
-                         <h2>
-                             {newsItem ? 'Редагувати' : 'Додати'}
-                             {' '}
-Новину
-                         </h2>
-                     </div>
-                     <Form.Item
-                         name="title"
-                         label="Заголовок: "
-                         rules={[{ required: true, message: 'Введіть заголовок' }]}
-                     >
-                         <Input maxLength={100} showCount />
-                     </Form.Item>
-
-                     <Form.Item
-                         name="url"
-                         label="Посилання: "
-                         rules={[{ required: true, message: 'Введіть Посилання' }]}
-                     >
-                         <Input maxLength={200} showCount />
-                     </Form.Item>
-
-                     <TinyMCEEditor
-                         onInit={(evt, editor) => {
-                             editorRef.current = editor;
-                         }}
-                         initialValue={newsItem ? newsItem.text : ''}
-                         init={{
-                             height: 300,
-                             menubar: false,
-                             plugins: [
-                                 'autolink',
-                                 'lists', 'preview', 'anchor', 'searchreplace', 'visualblocks',
-                                 'insertdatetime', 'wordcount',
-                             ],
-                             toolbar: 'undo redo | bold italic | '
-                                     + 'removeformat ',
-                             content_style: 'body { font-family:Roboto,Helvetica Neue,sans-serif; font-size:14px }',
-                         }}
-                     />
-
-                     <Form.Item
-                         name="image"
-                         label="Картинка"
-                         valuePropName="fileList"
-                         getValueFromEvent={(e: any) => {
-                             if (Array.isArray(e)) {
-                                 return e;
-                             }
-                             return e?.fileList;
-                         }}
-                     >
-                         <FileUploader
-                             multiple={false}
-                             accept=".jpeg,.png,.jpg"
-                             listType="picture-card"
-                             maxCount={1}
-                             onPreview={handlePreview}
-                             uploadTo="image"
-                             onSuccessUpload={(image:Image) => {
-                                 imageId.current = image.id;
-                                 newsItem.image = image;
+                     <div className="modalContainer-content">
+                         <Form
+                             form={form}
+                             layout="vertical"
+                             onFinish={onSuccessfulSubmitNews}
+                             initialValues={{
+                                 title: newsItem?.title,
+                                 url: newsItem?.url,
+                                 creationDate: newsItem ? dayjs(newsItem.creationDate) : dayjs(),
                              }}
-                             onRemove={(image) => {
-                                 newsItem.image = undefined;
-                             }}
-                             defaultFileList={(newsItem)
-                                 ? [{ name: '',
-                                      thumbUrl: base64ToUrl(newsItem.image?.base64, newsItem.image?.mimeType),
-                                      uid: newsItem.imageId?.toString(),
-                                      status: 'done' }]
-                                 : []}
                          >
-                             <p>Виберіть чи перетягніть файл</p>
-                         </FileUploader>
-                     </Form.Item>
-                     <PreviewFileModal opened={previewOpen} setOpened={setPreviewOpen} file={filePreview} />
-                     <div className="center">
-                         <Button className="streetcode-custom-button" onClick={() => form.submit()}>
+                             <div className="center">
+                                 <h2>
+                                     {newsItem ? 'Редагувати' : 'Додати'}
+                                     {' '}
+Новину
+                                 </h2>
+                             </div>
+                             <Form.Item
+                                 name="title"
+                                 label="Заголовок: "
+                                 rules={[{ required: true, message: 'Введіть заголовок' }]}
+                             >
+                                 <Input maxLength={100} showCount />
+                             </Form.Item>
+
+                             <Form.Item
+                                 name="url"
+                                 label="Посилання: "
+                                 rules={[{ required: true, message: 'Введіть Посилання' }]}
+                             >
+                                 <Input maxLength={200} showCount />
+                             </Form.Item>
+
+                             <TinyMCEEditor
+                                 onInit={(evt, editor) => {
+                                     editorRef.current = editor;
+                                 }}
+                                 initialValue={newsItem ? newsItem.text : ''}
+                                 init={{
+                                     height: 300,
+                                     menubar: false,
+                                     plugins: [
+                                         'autolink',
+                                         'lists', 'preview', 'anchor', 'searchreplace', 'visualblocks',
+                                         'insertdatetime', 'wordcount',
+                                     ],
+                                     toolbar: 'undo redo | bold italic | '
+                                     + 'removeformat ',
+                                     content_style: 'body { font-family:Roboto,Helvetica Neue,sans-serif; font-size:14px }',
+                                 }}
+                             />
+
+                             <Form.Item
+                                 name="image"
+                                 label="Картинка"
+                                 valuePropName="fileList"
+                                 getValueFromEvent={(e: any) => {
+                                     if (Array.isArray(e)) {
+                                         return e;
+                                     }
+                                     return e?.fileList;
+                                 }}
+                             >
+                                 <FileUploader
+                                     multiple={false}
+                                     accept=".jpeg,.png,.jpg"
+                                     listType="picture-card"
+                                     maxCount={1}
+                                     onPreview={handlePreview}
+                                     uploadTo="image"
+                                     onSuccessUpload={(image:Image) => {
+                                         imageId.current = image.id;
+                                         newsItem.image = image;
+                                     }}
+                                     onRemove={(image) => {
+                                         newsItem.image = undefined;
+                                     }}
+                                     defaultFileList={(newsItem)
+                                         ? [{ name: '',
+                                              thumbUrl: base64ToUrl(newsItem.image?.base64, newsItem.image?.mimeType),
+                                              uid: newsItem.imageId?.toString(),
+                                              status: 'done' }]
+                                         : []}
+                                 >
+                                     <p>Виберіть чи перетягніть файл</p>
+                                 </FileUploader>
+                             </Form.Item>
+                             <Form.Item
+                                 name="creationDate"
+                                 label="Дата створення: "
+                             >
+                                 <DatePicker />
+                             </Form.Item>
+                             <PreviewFileModal opened={previewOpen} setOpened={setPreviewOpen} file={filePreview} />
+                             <div className="center">
+                                 <Button className="streetcode-custom-button" onClick={() => form.submit()}>
                              Зберегти
-                         </Button>
+                                 </Button>
+                             </div>
+                         </Form>
                      </div>
-                 </Form>
-             </div>
-         </Modal>
+                 </Modal>
+             </ConfigProvider>
+         </div>
      );
  });
 export default NewsModal;
