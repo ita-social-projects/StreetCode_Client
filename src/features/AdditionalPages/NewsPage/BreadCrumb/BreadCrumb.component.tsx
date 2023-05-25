699ae5e981c9e507fc6f1105a9f5a3eb32167236
Lolimkeri <57957843+Lolimkeri@users.noreply.github.com>
2023-05-25 21:55:17 +0300

    Update BreadCrumb.component.tsx

--- src/features/AdditionalPages/NewsPage/BreadCrumb/BreadCrumb.component.tsx
+++ src/features/AdditionalPages/NewsPage/BreadCrumb/BreadCrumb.component.tsx
@@ -1,8 +1,6 @@
 import './BreadCrumb.styles.scss';
 
 import { Breadcrumb } from 'antd';
-
-import Streetcode from '@/models/streetcode/streetcode-types.model';
 import News from '@/models/news/news.model';
 
 
@@ -17,10 +15,6 @@ const BreadCrumb = ({ separator = '->', news }: Props) => (
             Головна
         </Breadcrumb.Item>
         <Breadcrumb.Item href="_blank">
-            {/* {streetcode?.firstName ?? streetcode?.title}
-            {' '}
-            {streetcode?.lastName} */}
-            {/* {news?.title} */}
             Новина
         </Breadcrumb.Item>
     </Breadcrumb>
