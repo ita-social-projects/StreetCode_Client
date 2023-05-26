bc6c7ee62ec07fb249c1648f2dfd01e7d561320d
Kateryna <92515141+Katerix@users.noreply.github.com>
2023-05-26 11:59:19 +0300

    Revert ngrok
    This reverts commit ac970cb0849f1ab4b9d553ada696ee3206914e7e.

--- src/app/api/donates/donation.api.ts
+++ src/app/api/donates/donation.api.ts
@@ -3,8 +3,7 @@ import Agent from '@api/agent.api';
 import { API_ROUTES } from '@constants/api-routes.constants';
 
 const DonationApi = {
-    create: (donation: Donation) => Agent.post<Donation>(`https://2a0d-91-196-178-227.ngrok-free.app/api/Payment/CreateInvoice`, donation)
+    create: (donation: Donation) => Agent.post<Donation>(`${API_ROUTES.DONATION.CREATE}`, donation)
 };
 
 export default DonationApi;
-//${API_ROUTES.DONATION.CREATE}
