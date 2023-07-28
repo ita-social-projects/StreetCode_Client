5f83787b02458feb30393fc3aa680d403422183a
Vitalii <98163405+MementoMorj@users.noreply.github.com>
2023-07-28 11:41:16 +0300

    search field fix

--- src/app/layout/header/SearchBlock/SearchResultItem/SearchItem.component.tsx
+++ src/app/layout/header/SearchBlock/SearchResultItem/SearchItem.component.tsx
@@ -1,7 +1,7 @@
 import './SearchItem.styles.scss';
 
 import { StreetcodeFilterResultDTO } from '@/models/filters/streetcode-filter.model';
-
+import htmlReactParser, { domToReact } from 'html-react-parser';
 interface Props {
     searchResultItem: StreetcodeFilterResultDTO;
 }
@@ -13,10 +13,12 @@ const SearchResultItem = ({ searchResultItem } : Props) => {
     const sourceName = `Стріткод #${searchResultItem.streetcodeIndex.toString().padStart(4, '0')
     }${searchResultItem.sourceName ? `/${searchResultItem.sourceName}` : ''}`;
 
+    const contentToDisplay = searchResultItem.content ? searchResultItem.content.slice(0, 67) : '';
+
     return (
         <a href={url} className="resultItemContainer">
             <div className="resultItemContent">
-                {searchResultItem.content}
+                 {htmlReactParser(contentToDisplay)}
             </div>
             <div className="resultSourceName">
                 {sourceName}
