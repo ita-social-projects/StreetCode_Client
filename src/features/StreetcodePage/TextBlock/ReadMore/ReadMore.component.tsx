2bffe1e09b6cff223dc51d33f911e7c68468ebcf
Maria Padalka <ms.padalka@gmail.com>
2023-08-27 22:40:37 +0300

    revert 6df9d5s commit

--- src/features/StreetcodePage/TextBlock/ReadMore/ReadMore.component.tsx
+++ src/features/StreetcodePage/TextBlock/ReadMore/ReadMore.component.tsx
@@ -1,55 +1,29 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 import './ReadMore.styles.scss';
 
-import { useEffect, useRef, useState } from 'react';
 import useToggle from '@hooks/stateful/useToggle.hook';
 import SearchTerms from '@streetcode/TextBlock/SearchTerms/SearchTerms.component';
-import lineHeight from 'line-height';
 
 import { moreTextEvent } from '@/app/common/utils/googleAnalytics.unility';
 
 interface Props {
-    text: string;
+  text: string;
+  maxTextLength?: number;
 }
 
-const ReadMore = ({ text }: Props) => {
+const ReadMore = ({ text, maxTextLength = 2e3 }: Props) => {
     const {
         toggleState: isReadMore,
         handlers: { toggle },
     } = useToggle(true);
 
-    const [isOverflowed, setIsOverflowed] = useState(false);
-    const textRef = useRef<HTMLDivElement>(null);
-    const VISIBLE_LINES = 25;
-
-    useEffect(() => {
-        const handleResize = () => {
-            if (textRef.current) {
-                const container = textRef.current;
-                const containerHeight = container.clientHeight;
-                const foundLineHeight = lineHeight(container);
-                const lines = Math.ceil(containerHeight / foundLineHeight);
-                setIsOverflowed(lines > VISIBLE_LINES);
-            }
-        };
-
-        handleResize();
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
     return (
         <>
-            {isOverflowed ? (
+            {(text.length > maxTextLength) ? (
                 <div className="text">
                     <div
                         className={isReadMore ? 'textShort' : undefined}
                         style={{ whiteSpace: 'pre-line' }}
-                        ref={textRef}
                     >
                         <SearchTerms mainText={text} />
                     </div>
@@ -66,7 +40,7 @@ const ReadMore = ({ text }: Props) => {
                     </div>
                 </div>
             ) : (
-                <div className="mainTextContent" ref={textRef}>
+                <div className="mainTextContent">
                     <SearchTerms mainText={text} />
                 </div>
             )}
