0201d7038dad471917cb6cabb50e1b243a4ac97b
maximu211 <63352037+maximu211@users.noreply.github.com>
2023-08-30 12:33:06 +0300

    first steps

--- src/features/AboutUsPage/TeamMembers/TeamMembers.component.tsx
+++ src/features/AboutUsPage/TeamMembers/TeamMembers.component.tsx
@@ -1,14 +1,26 @@
-import useWindowSize from "@/app/common/hooks/stateful/useWindowSize.hook";
-import { useEffect, useRef, useState } from "react";
-import './TeamMembers.component.scss'
-import BlockSlider from '@/features/SlickSlider/SlickSlider.component';
-import Picture from '@/assets/images/about-us/pictureWithBg2.png';
-import TeamMemberCard from "../TeamMemberCard/TeamMemberCard.component";
-import TeamMemberList from "./TeamMembersList/TeamMemberList.component";
-import TeamMember from "@/models/team/team.model";
-import TeamApi from "@/app/api/team/team.api";
+import './TeamMembers.component.scss';
 
-const TeamMembers = () => { 
+import React, { useEffect, useState } from 'react';
+import SwiperCore, {
+    A11y,
+    EffectFade,
+    Navigation,
+    Pagination,
+    Scrollbar,
+} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import TeamApi from '@/app/api/team/team.api';
+import Picture from '@/assets/images/about-us/pictureWithBg2.png';
+import TeamMember from '@/models/team/team.model';
+
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/css/pagination';
+import 'swiper/css';
+
+import TeamMemberList from './TeamMembersList/TeamMemberList.component';
+
+const TeamMembers = () => {
     const teamMemberRoles1: TeamMember[] = [
         {
             id: 0,
@@ -17,48 +29,62 @@ const TeamMembers = () => {
             description: 'Description',
             imageId: 1,
             image: {
-                id:1,
+                id: 1,
                 blobName: 'hueta',
-                mimeType:'popa',
-                base64: Picture
+                mimeType: 'popa',
+                base64: Picture,
             },
             teamMemberLinks: [],
             positions: [],
-        }, 
-    ]; 
+        },
+    ];
 
-    const [team, setTeam] = useState<TeamMember[]>([]); 
+    const [team, setTeam] = useState<TeamMember[]>([]);
     useEffect(
-        ()=>{
+        () => {
             TeamApi.getByRoleId(1).then(
-                result => setTeam(result)
+                (result) => setTeam(result),
             );
         },
-        []
+        [],
     );
 
-    const stringsArray: String[] = ['string1','string2','stringaaa3','stringgggg4','stringaa5','st6ringggaaaa66',]
+    const stringsArray: string[] = ['розробники', 'Історики', 'Керівники напрямів', 'Адміністратори', 'Засновники'];
 
-    const sliderItems = stringsArray.map(
-        (string) => (<div>{string}</div>)
-    );
-    
-    const sliderProps = {
-        infinite: true,
-        slidesToShow: 3,
-        arrows: true,
-        centerMode: true,
-        dots: false,
-    };
+    const sliderItems = stringsArray.map((string, index) => (
+        <React.Fragment key={index}>
+            <div>{string}</div>
+            <div className="square" />
+        </React.Fragment>
+    ));
 
     return (
-        <div className='aboutUsBlockContainer'>
-            <h1><div /><text>тут буде слайдер</text><div /></h1>
-            <BlockSlider {...sliderProps}>
-                {sliderItems}
-            </BlockSlider>
-            <TeamMemberList teamMembers = {team} />
+        <div className="aboutUsBlockContainer">
+            <h1>
+                <div />
+                <text>тут буде слайдер</text>
+                <div />
+            </h1>
+            <div className="topSliderContainer">
+                <div className="topSlider">
+                    <button className="arrow-right arrow">Prev</button>
+                    <Swiper
+                        slidesPerView={5}
+                        spaceBetween={30}
+                        centeredSlides
+                        loop
+                        navigation={{ nextEl: '.arrow-left', prevEl: '.arrow-right' }}
+                        modules={[Navigation]}
+                    >
+                        {stringsArray.map((item, index) => (
+                            <SwiperSlide key={index}>{item}</SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <button className="arrow-left arrow">Next</button>
+                </div>
+            </div>
+            <TeamMemberList teamMembers={team} />
         </div>
     );
-}
-export default TeamMembers; 
+};
+export default TeamMembers;
