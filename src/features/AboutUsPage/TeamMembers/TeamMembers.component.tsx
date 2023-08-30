7ef9de0acdbc36aa3bffcafed1acf0f030415494
maximu211 <63352037+maximu211@users.noreply.github.com>
2023-08-30 22:43:32 +0300

    Stashed sliders

--- src/features/AboutUsPage/TeamMembers/TeamMembers.component.tsx
+++ src/features/AboutUsPage/TeamMembers/TeamMembers.component.tsx
@@ -1,10 +1,6 @@
 import './TeamMembers.component.scss';
 
 import { useEffect, useState } from 'react';
-import LeftSliderArrow from '@assets/images/utils/LeftDefaultSliderArrow.svg';
-import RightSliderArrow from '@assets/images/utils/RightDefaultSliderArrow.svg';
-import { Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
 import PositionsApi from '@/app/api/team/positions.api';
 import TeamApi from '@/app/api/team/team.api';
@@ -18,19 +14,24 @@ import 'swiper/css';
 import TeamMemberCard from '../TeamMemberCard/TeamMemberCard.component';
 
 import TeamMemberList from './TeamMembersList/TeamMemberList.component';
+import TeamMemberSlider from './TeamPositionsSlider/TeamMembersSlider.component';
+import SliderComponents from './TeamPositionsSlider/TeamPostionSlider.component';
 
 const TeamMembers = () => {
     const [positions, setPositions] = useState<Positions[]>([]);
 
     const [team, setTeam] = useState<TeamMember[]>([]);
+    const [positionId, setPositionId] = useState<number>(-1);
 
     useEffect(
         () => {
-            TeamApi.getByRoleId(1).then(
-                (result) => setTeam(result),
-            ).then();
+            if (positionId > 0) {
+                TeamApi.getByRoleId(positionId).then(
+                    (result) => setTeam(result),
+                );
+            }
         },
-        [],
+        [positionId],
     );
 
     useEffect(() => {
@@ -39,92 +40,22 @@ const TeamMembers = () => {
         });
     }, []);
 
-    const [swiper, setSwiper] = useState(null);
-
-    const handlePreviousSlide = () => {
-        if (swiper) {
-            swiper.slidePrev();
-        }
-    };
-
-    const handleNextSlide = () => {
-        if (swiper) {
-            swiper.slideNext();
-        }
-    };
-
     const sliderItems = positions.map((position, index) => (
         <div key={index}>
             <div>{position.position}</div>
         </div>
     ));
 
-    const getSliderData = () => {
-        if (team.length > 0) {
-            return (
-                <Swiper
-                    slidesPerView="auto"
-                    centeredSlides
-                    spaceBetween={20}
-                    slideToClickedSlide
-                    pagination={window.innerWidth < SCREEN_SIZES.phone}
-                    loop
-                >
-                    {team.map(
-                        (member) => (
-                            <SwiperSlide>
-                                <TeamMemberCard {...member} />
-                            </SwiperSlide>
-                        ),
-                    )}
-                </Swiper>
-            );
-        }
-        return (<></>);
-    };
-
     return (
         <div className="aboutUsBlockContainer">
-            <div className="topSliderContainer">
-                <LeftSliderArrow className="slider-arrow" alt="Previous" onClick={() => handlePreviousSlide()} />
-                <div className="topSlider">
-                    <Swiper
-                        className="squareParent"
-                        onSwiper={(swiper) => setSwiper(swiper)}
-                        slidesPerView={5}
-                        spaceBetween={0}
-                        centeredSlides
-                        loop
-                        navigation={{ nextEl: '.arrow-left', prevEl: '.arrow-right' }}
-                        modules={[Navigation]}
-                        breakpoints={{
-                            360: {
-                                slidesPerView: 1,
-                            },
-                            425: {
-                                slidesPerView: 3,
-                            },
-                            768: {
-                                slidesPerView: 5,
-                            },
-                        }}
-                    >
-                        {sliderItems.map((item, index) => (
-                            <SwiperSlide className="square" key={index}>{item}</SwiperSlide>
-                        ))}
-                    </Swiper>
-                </div>
-                <RightSliderArrow className="slider-arrow" alt="Next" onClick={() => handleNextSlide()} />
-            </div>
-            { window.innerWidth <= SCREEN_SIZES.tablet
-                ? (
-                    <div className="teamMembersSlider">
-                        {
-                            getSliderData()
-                        }
-                    </div>
-                )
-                : <TeamMemberList teamMembers={team} />}
+            <SliderComponents
+                sliderItems={sliderItems}
+            />
+            {window.innerWidth <= SCREEN_SIZES.tablet ? (
+                <TeamMemberSlider team={team} />
+            ) : (
+                <TeamMemberList teamMembers={team} />
+            )}
         </div>
     );
 };
