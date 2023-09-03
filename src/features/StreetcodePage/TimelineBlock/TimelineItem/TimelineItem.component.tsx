e53fa7cc9e1940c87df568464b90dda5138f4dfe
vvbogdanovih <vvbogdanovih@gmail.com>
2023-09-03 18:24:36 +0300

    Revert "Fixed month format on chronologi"
    This reverts commit faa17cb25deb6fcc0f099223cb07dc2641d4b721.

--- src/features/StreetcodePage/TimelineBlock/TimelineItem/TimelineItem.component.tsx
+++ src/features/StreetcodePage/TimelineBlock/TimelineItem/TimelineItem.component.tsx
@@ -5,24 +5,6 @@ import { uk } from 'date-fns/locale';
 import dayjs from 'dayjs';
 
 import TimelineItem, { DateViewPattern, getSeason } from '@/models/timeline/chronology.model';
-const monthsMap = new Map([
-    ["січня", "січень"],
-    ["лютого", "лютий"],
-    ["березня", "березень"],
-    ["квітня", "квітень"],
-    ["травня", "травень"],
-    ["червня", "червень"],
-    ["липня", "липень"],
-    ["серпня", "серпень"],
-    ["вересня", "вересень"],
-    ["жовтня", "жовтень"],
-    ["листопада", "листопад"],
-    ["грудня", "грудень"]
-]);
-
-const months = ["січня", "лютого", "березня", "квітня",
-    "травня", "червня", "липня", "серпня",
-    "вересня", "жовтня", "листопада", "грудня"];
 
 const truncateCharString = (str?: string, symbolCount = 400): string | undefined => str && (
     str.length <= symbolCount ? str : `${str.substring(0, symbolCount - 3)}...`
@@ -31,13 +13,13 @@ const truncateCharString = (str?: string, symbolCount = 400): string | undefined
 interface Props {
     timelineItem: TimelineItem;
 }
-function FromDateToString(date: Date, dataViewType: DateViewPattern) {
+function FromDateToString(date: Date, dataViewType:DateViewPattern) {
     switch (dataViewType) {
-        case DateViewPattern.Year: return format(new Date(date), 'yyyy');
-        case DateViewPattern.MonthYear: return format(date, 'yyyy, MMMM', { locale: uk });
-        case DateViewPattern.SeasonYear: return `${getSeason(dayjs(date))} ${format(date, 'yyyy')}`;
-        case DateViewPattern.DateMonthYear: return format(new Date(date), 'yyyy, d MMMM', { locale: uk });
-        default: return '';
+    case DateViewPattern.Year: return format(new Date(date), 'yyyy');
+    case DateViewPattern.MonthYear: return format(date, 'yyyy, MMMM', { locale: uk });
+    case DateViewPattern.SeasonYear: return `${getSeason(dayjs(date))} ${format(date, 'yyyy')}`;
+    case DateViewPattern.DateMonthYear: return format(new Date(date), 'yyyy, d MMMM', { locale: uk });
+    default: return '';
     }
 }
 const TimelineSlideCard = ({
@@ -45,37 +27,25 @@ const TimelineSlideCard = ({
         date, description,
         historicalContexts, title, dateViewPattern,
     },
-}: Props) => {
-    let newDate: string;
-    if (dateViewPattern == 0 || dateViewPattern == 1) {
-        var words = FromDateToString(new Date(date), dateViewPattern).split(" ");
-        newDate = words[0] + " " + words[1] + " " + monthsMap.get(words[2]);
-    }
-    else{
-        newDate = FromDateToString(new Date(date), dateViewPattern)
-    }
-
-
-    return (
-        <div className="timelineItem">
-            <div className="timelineItemContent">
-                <p className="timelineItemMetadata">
-                    {newDate}
-                    {historicalContexts.map(({ id, title: ctxTitle }) => (
-                        <span key={id} className="historicalContext">
-                            {`. ${ctxTitle}`}
-                        </span>
-                    ))}
-                </p>
-                <p className="timelineItemTitle">
-                    {title}
-                </p>
-                <p className="timelineItemDescription">
-                    {truncateCharString(description)}
-                </p>
-            </div>
+} : Props) => (
+    <div className="timelineItem">
+        <div className="timelineItemContent">
+            <p className="timelineItemMetadata">
+                {FromDateToString(new Date(date), dateViewPattern)}
+                {historicalContexts.map(({ id, title: ctxTitle }) => (
+                    <span key={id} className="historicalContext">
+                        {`. ${ctxTitle}`}
+                    </span>
+                ))}
+            </p>
+            <p className="timelineItemTitle">
+                {title}
+            </p>
+            <p className="timelineItemDescription">
+                {truncateCharString(description)}
+            </p>
         </div>
-    )
-};
+    </div>
+);
 
 export default TimelineSlideCard;
