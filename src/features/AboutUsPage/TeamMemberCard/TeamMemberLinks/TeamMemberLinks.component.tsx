d10318c639c4eaa1f28840b068b8200325eb3a33
Kuzmych Roman <67451349+Dobriyr@users.noreply.github.com>
2023-09-05 22:46:54 +0300

    change pnd to webp

--- src/features/AboutUsPage/TeamMemberCard/TeamMemberLinks/TeamMemberLinks.component.tsx
+++ src/features/AboutUsPage/TeamMemberCard/TeamMemberLinks/TeamMemberLinks.component.tsx
@@ -1,7 +1,7 @@
-import facebook from '@assets/images/partners/facebook.png';
-import instagram from '@assets/images/partners/instagram.png';
-import twitter from '@assets/images/partners/twitter.png';
-import youtube from '@assets/images/partners/youtube.png';
+import facebook from '@assets/images/partners/facebook.webp';
+import instagram from '@assets/images/partners/instagram.webp';
+import twitter from '@assets/images/partners/twitter.webp';
+import youtube from '@assets/images/partners/youtube.webp';
 
 import { TeamMemberLinkCreateUpdate } from '@/models/team/team.model';
 
