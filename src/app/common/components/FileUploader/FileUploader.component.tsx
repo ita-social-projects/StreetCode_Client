127ae344e7dbbeb7ae1be939ad49b73a7f004caa
Oleh-Kril <oleh.kril.t@gmail.com>
2023-09-15 17:33:18 +0300

    - remove fileList from CategoryAdminModal - merge develop to branch

--- src/app/common/components/FileUploader/FileUploader.component.tsx
+++ src/app/common/components/FileUploader/FileUploader.component.tsx
@@ -4,7 +4,7 @@ import Upload, { RcFile, UploadChangeParam, UploadFile, UploadProps } from 'antd
 
 import AudiosApi from '@/app/api/media/audios.api';
 import ImagesApi from '@/app/api/media/images.api';
-import Audio, { AudioCreate, AudioUpdate } from '@/models/media/audio.model';
+import Audio, { AudioCreate } from '@/models/media/audio.model';
 import ImageCustom, { ImageCreate } from '@/models/media/image.model';
 
 type UploaderWithoutChildren = Omit<UploadProps, 'children'>;
@@ -17,19 +17,22 @@ interface Props extends UploaderWithoutChildren {
     greyFilterForImage: boolean;
     onSuccessUpload?:(value:ImageCustom | Audio)=>void;
 }
-const FileUploader:React.FC<Props> = ({ onSuccessUpload, uploadTo, greyFilterForImage = false, children, ...uploadProps }) => {
+const FileUploader:React.FC<Props> = ({
+    onSuccessUpload, uploadTo, greyFilterForImage = false, children, ...uploadProps
+}) => {
     const imageOrAudioDataAsURL = useRef<any[]>([]);
-    const imageDataAsURL = useRef<any | null>(null);
     const onUploadChange = (uploadParams: UploadChangeParam<UploadFile<any>>) => {
-        if(!imageOrAudioDataAsURL.current.find(el => el.uid === uploadParams.file.uid)){
+        if (!imageOrAudioDataAsURL.current.find((el) => el.uid === uploadParams.file.uid)) {
             const reader = new FileReader();
             reader.onloadend = (obj) => {
                 let base64 = obj.target?.result;
 
                 if (greyFilterForImage && base64) {
                     const img = new Image();
-                    img.src = base64;
-                    if (img.height > 0 && img.width > 0) {
+                    if (typeof base64 === 'string') {
+                        img.src = base64;
+                    }
+                    if (img.height > 0 && img.width > 0 && img.src) {
                         const canvas = document.createElement('canvas');
                         const context = canvas.getContext('2d');
                         if (context !== null) {
@@ -39,7 +42,7 @@ const FileUploader:React.FC<Props> = ({ onSuccessUpload, uploadTo, greyFilterFor
                             context.drawImage(img, 0, 0);
 
                             const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-                            const {data} = imageData;
+                            const { data } = imageData;
 
                             for (let i = 0; i < data.length; i += 4) {
                                 const red = data[i];
@@ -59,7 +62,14 @@ const FileUploader:React.FC<Props> = ({ onSuccessUpload, uploadTo, greyFilterFor
                         }
                     }
                 }
-                imageOrAudioDataAsURL.current.push({base64: base64, name: uploadParams.file.name, uid: uploadParams.file.uid});
+
+                imageOrAudioDataAsURL.current.push(
+                    {
+                        base64,
+                        name: uploadParams.file.name,
+                        uid: uploadParams.file.uid,
+                    },
+                );
             };
             if (uploadParams.fileList.length === 0) {
                 imageOrAudioDataAsURL.current = [];
@@ -74,24 +84,28 @@ const FileUploader:React.FC<Props> = ({ onSuccessUpload, uploadTo, greyFilterFor
             }
         }
     };
+
     const onFileUpload = (uploadType:'image' | 'audio', uplFile:UploadFile)
     :Promise< ImageCustom | Audio> => {
         const imageOrAudioData = imageOrAudioDataAsURL.current.filter((el: any) => el.name === uplFile.name)[0]?.base64;
 
         const imageOrAudio: ImageCreate = {
-            baseFormat: imageOrAudioData?.substring(imageOrAudioData?.indexOf(',') + 1, imageOrAudioData?.length),
+            baseFormat: imageOrAudioData?.substring(
+                (imageOrAudioData?.indexOf(',') || 0) + 1,
+                imageOrAudioData?.length,
+            ),
             extension: uplFile.name.substring(uplFile.name
                 .lastIndexOf('.') + 1, uplFile.name.length),
             mimeType: uplFile.type!,
-            title: uplFile.name
+            title: uplFile.name,
         };
 
         if (uploadType === 'audio') {
             return AudiosApi.create(imageOrAudio as AudioCreate);
-        }else{
-            return ImagesApi.create(imageOrAudio as ImageCreate);
         }
+        return ImagesApi.create(imageOrAudio as ImageCreate);
     };
+
     const customRequest = async (options:any) => {
         const {
             onSuccess, onError, file, action, onProgress,
