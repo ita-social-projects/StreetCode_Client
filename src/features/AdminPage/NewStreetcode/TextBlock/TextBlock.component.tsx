e59bd1a320b46e1f23c47a4ea08a4de2d3450d87
Adeodonne <bbozhik284@gmail.com>
2023-09-18 02:25:11 +0300

    fix tox in streetcode edit window

--- src/features/AdminPage/NewStreetcode/TextBlock/TextBlock.component.tsx
+++ src/features/AdminPage/NewStreetcode/TextBlock/TextBlock.component.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-restricted-imports */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TextsApi from '@api/streetcode/text-content/texts.api';
+import { useAsync } from '@hooks/stateful/useAsync.hook';
 
 import Video from '@/models/media/video.model';
 import { Text } from '@/models/streetcode/text-contents.model';
@@ -12,10 +14,26 @@ interface Props {
     video: Video | undefined;
     setVideo: React.Dispatch<Video | undefined>;
     onChange: (fieldName: string, value: any) => void;
+    parseId: number
 }
 
-const TextBlock = React.memo(({ inputInfo, setInputInfo, video, setVideo, onChange }: Props) => (
-    <TextForm inputInfo={inputInfo} setInputInfo={setInputInfo} video={video} setVideo={setVideo} onChange={onChange} />
-));
+const TextBlock = React.memo(({
+    inputInfo, setInputInfo, video, setVideo, onChange, parseId,
+}: Props) => {
+    const [inputInfoAsync, setInputInfoAsync] = useState<Partial<Text>>();
+    useAsync(() => {
+        TextsApi.getByStreetcodeId(parseId).then((result) => {
+            setInputInfoAsync(result);
+        });
+    });
+
+    useEffect(() => {
+        setInputInfo(inputInfoAsync);
+    }, [inputInfoAsync]);
+
+    return (
+        <TextForm inputInfo={inputInfoAsync} setInputInfo={setInputInfoAsync} video={video} setVideo={setVideo} onChange={onChange} />
+    );
+});
 
 export default TextBlock;
