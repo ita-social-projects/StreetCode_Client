d0c573ee7b76990197127e06ac6e614544920ccb
KateYatsiuk <katerynayatsiuk@gmail.com>
2023-09-18 20:23:43 +0300

    Fixed audio

--- src/features/AdminPage/NewStreetcode/MainNewStreetcode.component.tsx
+++ src/features/AdminPage/NewStreetcode/MainNewStreetcode.component.tsx
@@ -29,13 +29,14 @@ import FRONTEND_ROUTES from '@/app/common/constants/frontend-routes.constants';
 import Subtitle, { SubtitleCreate } from '@/models/additional-content/subtitles.model';
 import { StreetcodeTag, StreetcodeTagUpdate } from '@/models/additional-content/tag.model';
 import StatisticRecord from '@/models/analytics/statisticrecord.model';
-import Image, { ImageAssigment, ImageCreateUpdate, ImageDetails } from '@/models/media/image.model';
+import { AudioUpdate } from '@/models/media/audio.model';
+import { ImageCreateUpdate, ImageDetails } from '@/models/media/image.model';
 import { StreetcodeArtCreateUpdate } from '@/models/media/streetcode-art.model';
 import Video, { VideoCreate } from '@/models/media/video.model';
 import { PartnerCreateUpdateShort, PartnerUpdate } from '@/models/partners/partners.model';
 import { StreetcodeCategoryContent, StreetcodeCategoryContentUpdate } from '@/models/sources/sources.model';
 import { StreetcodeCreate, StreetcodeType, StreetcodeUpdate } from '@/models/streetcode/streetcode-types.model';
-import { Fact, FactCreate, Text, TextCreateUpdate } from '@/models/streetcode/text-contents.model';
+import { Fact, Text, TextCreateUpdate } from '@/models/streetcode/text-contents.model';
 import TransactionLink from '@/models/transactions/transaction-link.model';
 
 import ARBlock from './ARBlock/ARBlock.component';
@@ -140,7 +141,7 @@ const NewStreetcode = () => {
     useEffect(() => {
         if (ukUA.DatePicker) {
             ukUA.DatePicker.lang.locale = 'uk';
-        }   
+        }
 
         if (parseId) {
             StreetcodeArtApi.getStreetcodeArtsByStreetcodeId(parseId).then((result) => {
@@ -328,10 +329,10 @@ const NewStreetcode = () => {
                 status: tempStatus,
                 toponyms: newStreetcodeInfoStore.selectedToponyms,
                 streetcodeCategoryContents:
-                JSON.parse(JSON.stringify(sourceCreateUpdateStreetcode.streetcodeCategoryContents))
-                    .map((streetcodeCategoryContent: StreetcodeCategoryContent) => (
-                        { ...streetcodeCategoryContent, id: 0 }
-                    )),
+                    JSON.parse(JSON.stringify(sourceCreateUpdateStreetcode.streetcodeCategoryContents))
+                        .map((streetcodeCategoryContent: StreetcodeCategoryContent) => (
+                            { ...streetcodeCategoryContent, id: 0 }
+                        )),
                 statisticRecords: JSON.parse(JSON.stringify(statisticRecordStore.getStatisticRecordArray))
                     .map((statisticRecord: StatisticRecord) => (
                         {
@@ -367,7 +368,7 @@ const NewStreetcode = () => {
                 const videosUpdate: Video[] = [{ ...video, url: inputInfo?.link ?? '' } as Video];
 
                 const subtitleUpdate: Subtitle[] = [
-                { ...subTitle, subtitleText: subTitle?.subtitleText ?? '' } as Subtitle];
+                    { ...subTitle, subtitleText: subTitle?.subtitleText ?? '' } as Subtitle];
 
                 const tags = [...(selectedTags as StreetcodeTagUpdate[])
                     .map((tag) => ({ ...tag, streetcodeId: parseId })),
@@ -420,8 +421,9 @@ const NewStreetcode = () => {
                     statisticRecords: statisticRecordStore.getStatisticRecordArrayToUpdate
                         .map((record) => ({ ...record, streetcodeId: parseId })),
                     toponyms: newStreetcodeInfoStore.selectedToponyms,
-                    images: createUpdateMediaStore.imagesUpdate.map((img):ImageCreateUpdate => ({ id: img.id, modelState: img.modelState, streetcodeId: img.streetcodeId })),
-                    audios: createUpdateMediaStore.audioUpdate,
+                    images: createUpdateMediaStore.imagesUpdate.map((img): ImageCreateUpdate => ({ id: img.id, modelState: img.modelState, streetcodeId: img.streetcodeId })),
+                    audioId: createUpdateMediaStore.audioId,
+                    audios: createUpdateMediaStore.audioUpdate.map((a): AudioUpdate => ({ id: a.id, modelState: a.modelState, streetcodeId: a.streetcodeId })),
                     transactionLink: {
                         id: arLink?.id ?? 0,
                         streetcodeId: parseId,
@@ -429,7 +431,7 @@ const NewStreetcode = () => {
                         qrCodeUrl: arLink?.urlTitle ?? '',
                         modelState: 0,
                     },
-                    imagesDetails: (Array.from(factsStore.factImageDetailsMap.values()) as ImageDetails []).concat(createUpdateMediaStore.getImageDetailsUpdate()),
+                    imagesDetails: (Array.from(factsStore.factImageDetailsMap.values()) as ImageDetails[]).concat(createUpdateMediaStore.getImageDetailsUpdate()),
                 };
 
                 if (streetcodeType.current === StreetcodeType.Person) {
