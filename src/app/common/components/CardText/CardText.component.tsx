33d36b8b5fb1fb3c71c0907ae58274540aed6b7a
Oleh-Kril <oleh.kril.t@gmail.com>
2023-11-07 15:56:39 +0200

    adapt shared component to wow-facts

--- src/app/common/components/CardText/CardText.component.tsx
+++ src/app/common/components/CardText/CardText.component.tsx
@@ -1,9 +1,9 @@
 import './CardText.styles.scss';
 
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 type Props = {
-    isMoreBtnOptional?: boolean
     moreBtnText?: string,
     className?: string,
     onBtnClick: any,
@@ -13,30 +13,21 @@ type Props = {
 };
 
 const CardText = ({
-    isMoreBtnOptional = false, moreBtnText = 'Трохи ще', title, text, subTitle, className, onBtnClick,
-}:Props) => {
-    useEffect(() => {
-
-    }, []);
-
-    return (
-        <div className={`cardTextContainer ${className}`}>
-            <div className="cardTextContainerTopPart">
-                <p className="cardTextContainerTitle">{title}</p>
-                {subTitle ? <p className="cardTextContainerSubTitle">{subTitle}</p> : <></>}
-                <p className="cardTextContainerText">{text}</p>
-            </div>
-
-            {!isMoreBtnOptional ? (
-                <p
-                    className="cardTextContainerButton"
-                    onClick={onBtnClick}
-                >
-                    {moreBtnText}
-                </p>
-            ) : <></>}
+    moreBtnText = 'Трохи ще...', title, text, subTitle, className, onBtnClick,
+}:Props) => (
+    <div className={`cardTextContainer ${className}`}>
+        <div className="cardTextContainerTopPart">
+            <p className="cardTextContainerTitle">{title}</p>
+            {subTitle ? <p className="cardTextContainerSubTitle">{subTitle}</p> : <></>}
+            <p className="cardTextContainerText">{text}</p>
         </div>
-    );
-};
+        <p
+            className="cardTextContainerButton"
+            onClick={onBtnClick}
+        >
+            {moreBtnText}
+        </p>
+    </div>
+);
 
 export default CardText;
