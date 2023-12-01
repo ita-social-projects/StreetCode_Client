277afc08defc1eda4d00b07a877b0efc36a0a815
Oleh-Kril <oleh.kril.t@gmail.com>
2023-12-01 13:53:33 +0200

    moved news file to another folder

--- server/src/controllers/client/services/news/news.service.ts
+++ server/src/controllers/client/services/news/news.service.ts
@@ -7,12 +7,12 @@ export class NewsService {
   constructor(private readonly httpService: HttpService) {}
 
   public async getAllNews(): Promise<AxiosResponse> {
-    const url = 'https://stageback.streetcode.com.ua/api/news/getAll';
+    const url = '/news/getAll';
     return this.httpService.get(url).toPromise();
   }
 
   public async getByUrl(newsUrl: string): Promise<AxiosResponse> {
-    const backUrl = 'https://stageback.streetcode.com.ua/api/news/getByUrl/';
+    const backUrl = '/news/getByUrl/';
     return this.httpService.get(backUrl + newsUrl).toPromise();
   }
 }
