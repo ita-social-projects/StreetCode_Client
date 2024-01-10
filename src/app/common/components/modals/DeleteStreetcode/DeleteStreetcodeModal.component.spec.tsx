ad6c164271e00fd03b89dfc76ded399961fffc04
Rominos7 <Rominos7@gmail.com>
2024-01-10 19:57:12 +0200

    fix_lint

--- src/app/common/components/modals/DeleteStreetcode/DeleteStreetcodeModal.component.spec.tsx
+++ src/app/common/components/modals/DeleteStreetcode/DeleteStreetcodeModal.component.spec.tsx
@@ -20,7 +20,7 @@ describe('DeleteStreetcodeModalComponent', () => {
     describe('using getElementsByClassName', () => {
         beforeEach(() => {
             const mockUseState: any = (initValue: any) => [initValue, mockSetState];
-            jest.spyOn(React, 'useState').mockImplementation(mockUseState);            
+            jest.spyOn(React, 'useState').mockImplementation(mockUseState);
         });
 
         afterEach(() => {
@@ -29,47 +29,47 @@ describe('DeleteStreetcodeModalComponent', () => {
 
         it('should render itself and its elements', () => {
             const { container } = render(<DeleteStreetcodeModalComponent />);
-        
+
             const title = container.getElementsByClassName('modalTitle');
             expect(title).toHaveLength(1);
             expect(title[0].textContent).toBe(TITLE);
-        
+
             const isModalOpen = container.getElementsByClassName('isModalOpen');
             expect(isModalOpen).toHaveLength(1);
             expect(isModalOpen[0].textContent).toBe(`${mockIsOpen}`);
-        
+
             const okButton = container.getElementsByClassName('modalOkButton');
             expect(okButton).toHaveLength(1);
-        
+
             const cancelButton = container.getElementsByClassName('modalCancelButton');
             expect(cancelButton).toHaveLength(1);
-        
+
             const modalChildren = container.getElementsByClassName('modal-children');
             expect(modalChildren).toHaveLength(1);
             expect(modalChildren[0].textContent).toEqual(`${mockID}`);
         });
-        
+
         it('should call setModal from useModalContext when clicking onClick button', () => {
             const { container } = render(<DeleteStreetcodeModalComponent />);
             const okButton = container.getElementsByClassName('modalOkButton')[0];
-        
+
             fireEvent.click(okButton);
             expect(mockSetModal).toHaveBeenCalledWith(DELETE_STREETCODE);
         });
-        
+
         it('should call setModal from useModalContext when clicking onCancel button', () => {
             const { container } = render(<DeleteStreetcodeModalComponent />);
             const cancelButton = container.getElementsByClassName('modalCancelButton')[0];
-        
+
             fireEvent.click(cancelButton);
             expect(mockSetState).toHaveBeenCalled();
         });
     });
 
-    describe('using getByRole', () => {        
+    describe('using getByRole', () => {
         beforeEach(() => {
             const mockUseState: any = (initValue: any) => [initValue, mockSetState];
-            jest.spyOn(React, 'useState').mockImplementation(mockUseState);            
+            jest.spyOn(React, 'useState').mockImplementation(mockUseState);
         });
 
         afterEach(() => {
@@ -81,21 +81,19 @@ describe('DeleteStreetcodeModalComponent', () => {
             const okButton = getByRole(container, 'button', {
                 name: /okButton/ // button text
             });
-        
+
             fireEvent.click(okButton);
             expect(mockSetModal).toHaveBeenCalledWith(DELETE_STREETCODE);
         });
-        
+
         it('should call setModal from useModalContext when clicking onCancel button', () => {
             const { container } = render(<DeleteStreetcodeModalComponent />);
             const cancelButton = getByRole(container, 'button', {
                 name: /cancelButton/ // button text
             });
-        
+
             fireEvent.click(cancelButton);
             expect(mockSetState).toHaveBeenCalled();
         });
     })
 });
-
-
