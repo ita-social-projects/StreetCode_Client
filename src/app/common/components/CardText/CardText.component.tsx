1efc8546647fa2e52f07b8c79c4f788b63ae7389
Oleh-Kril <oleh.kril.t@gmail.com>
2024-01-15 11:38:48 +0200

    refactor StreetcodeSliderItem on main page and NewsSliderItem on main page to use Link component

--- src/app/common/components/CardText/CardText.component.tsx
+++ src/app/common/components/CardText/CardText.component.tsx
@@ -1,19 +1,19 @@
 import './CardText.styles.scss';
 
-import { useEffect, useMemo } from 'react';
-import { useMediaQuery } from 'react-responsive';
+import { Link } from 'react-router-dom';
 
 type Props = {
     moreBtnText?: string,
+    moreBtnAsLink?: { link: string, state: any },
     className?: string,
-    onBtnClick: any,
+    onBtnClick?: (event: any) => void,
     title: string
     subTitle?: string,
     text: string
 };
 
 const CardText = ({
-    moreBtnText = 'Трохи ще...', title, text, subTitle, className, onBtnClick,
+    moreBtnText = 'Трохи ще...', title, text, subTitle, className, onBtnClick, moreBtnAsLink,
 }:Props) => (
     <div className={`cardTextContainer ${className}`}>
         <div className="cardTextContainerTopPart">
@@ -21,12 +21,25 @@ const CardText = ({
             {subTitle ? <p className="cardTextContainerSubTitle">{subTitle}</p> : <></>}
             <p className="cardTextContainerText">{text}</p>
         </div>
-        <p
-            className="cardTextContainerButton"
-            onClick={onBtnClick}
-        >
-            {moreBtnText}
-        </p>
+        {moreBtnAsLink
+            ? (
+                <Link
+                    to={moreBtnAsLink.link}
+                    state={moreBtnAsLink.state}
+                    className="cardTextContainerButton"
+                >
+                    {moreBtnText}
+                </Link>
+            )
+            : (
+                <p
+                    className="cardTextContainerButton"
+                    onClick={onBtnClick}
+                >
+                    {moreBtnText}
+                </p>
+            )}
+
     </div>
 );
 
