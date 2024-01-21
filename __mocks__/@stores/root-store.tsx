c35d3338ea9cabe0870e4d90481259c1069415e3
Rominos7 <Rominos7@gmail.com>
2024-01-21 23:51:09 +0200

    expand_root-store-mock

--- __mocks__/@stores/root-store.tsx
+++ __mocks__/@stores/root-store.tsx
@@ -1,6 +1,8 @@
 export const mockID = 1;
 export const mockIsOpen = true;
 export const mockSetModal = jest.fn();
+export const mockUpdateTeam = jest.fn();
+export const mockCreateTeam = jest.fn();
 
 export const useModalContext = () => {
     return {
@@ -14,4 +16,21 @@ export const useModalContext = () => {
             }
         } 
     }
-}
+};
+
+export const useMobx = () => {
+    return {
+        teamStore: {
+            updateTeam: mockUpdateTeam,
+            createTeam: mockCreateTeam,
+            getTeamArray: [
+                {
+                    name: 'name',
+                    imageId: 1,
+                }
+            ]
+        }
+    }
+};
+
+export default useMobx;
