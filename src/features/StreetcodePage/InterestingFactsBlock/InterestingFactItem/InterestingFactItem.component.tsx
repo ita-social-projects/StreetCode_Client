ab739e183505f9c39ee52a27f30762ab0ba41a95
mykola-krai <133663112+mykola-krai@users.noreply.github.com>
2024-01-24 11:08:42 +0200

    Two-line title for interesting facts (#1116)
    * Two-line title for interesting facts
    
    * fix layout on streetcode edit/create (#1108)
    
    * refactor StreetcodeSliderItem on main page and NewsSliderItem on main page to use Link component (#1110)
    
    * remove "Оживити картинку" button (#1111)
    
    * Links open in new tabs (#1118)
    
    * Links open in new tabs
    
    * Cleaned code
    
    * Ordered/unordered lists are displayed now (#1119)
    
    * remove unused fields and imports (no logic modified)
    
    * Two-line title for interesting facts
    
    * remove unused fields and imports (no logic modified)
    
    ---------
    
    Co-authored-by: Oleh-Kril <91199925+Oleh-Kril@users.noreply.github.com>
    Co-authored-by: Kateryna Yatsiuk <113370393+KateYatsiuk@users.noreply.github.com>
    Co-authored-by: Oleh-Kril <oleh.kril.t@gmail.com>

--- src/features/StreetcodePage/InterestingFactsBlock/InterestingFactItem/InterestingFactItem.component.tsx
+++ src/features/StreetcodePage/InterestingFactsBlock/InterestingFactItem/InterestingFactItem.component.tsx
@@ -6,26 +6,21 @@ import CardText from '@components/CardText/CardText.component';
 import { Fact } from '@models/streetcode/text-contents.model';
 import { useModalContext } from '@stores/root-store';
 
+// eslint-disable-next-line import/extensions
 import useIsVisible from '@/app/common/hooks/stateful/useIsVisible';
-import useWindowSize from '@/app/common/hooks/stateful/useWindowSize.hook';
 import base64ToUrl from '@/app/common/utils/base64ToUrl.utility';
 
 interface Props {
     fact: Fact;
-    maxTextLength?: number;
-    numberOfSlides: number;
     index?: number;
     middleFactIndex?: number;
 }
 
 const InterestingFactItem = ({
     fact: { factContent, title, id, image },
-    maxTextLength = 250,
-    numberOfSlides,
     index,
     middleFactIndex,
 }: Props) => {
-    const windowSize = useWindowSize();
     const millisecondsToHideAfterOpening = 4000;
     const { modalStore } = useModalContext();
     const timeout = useRef<NodeJS.Timeout>();
