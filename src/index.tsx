26bbaa4ba4fbaf0f48da850f32126e041edbcdbe
Oleh-Kril <oleh.kril.t@gmail.com>
2024-01-25 09:32:06 +0200

    declare global window type

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,16 @@ import ReactGA from 'react-ga4';
 import { RouterProvider } from 'react-router-dom';
 import router from '@app/router/Routes';
 
+declare global {
+    interface Window {
+        _env_: {
+            API_URL: string;
+            SERVER_API_URL: string;
+            REACT_APP_GOOGLE_ANALYTICS: string;
+        };
+    }
+}
+
 ReactGA.initialize('G-2RHY04JKG0');
 
 const root = ReactDOM.createRoot(
