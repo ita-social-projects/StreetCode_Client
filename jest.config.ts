b3ccd43090f2c86850feb52fd8475a783ef2e7b2
Dmytro <dimakhved@gmail.com>
2024-03-03 12:37:25 +0200

    add: test for necessary fields, todo: test for all fields + refactor

--- jest.config.ts
+++ jest.config.ts
@@ -1,63 +1,66 @@
-import type { JestConfigWithTsJest } from 'ts-jest';
+import type { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    roots: ['<rootDir>'],
-    moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-        '^@assets/(.*)$': '<rootDir>/src/assets/$1',
-        // PartnerModal specification for testing
-        'antd': '<rootDir>/__mocks__/antd/antd',
-        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.ts',
-        '^@/app/common/components/FileUploader/(.*)$': '<rootDir>/src/app/common/components/FileUploader/$1',
-        '^@/app/common/utils/(.*)$': '<rootDir>/src/app/common/utils/$1',
-        '^@features/AdminPage/NewStreetcode/MainBlock/PreviewFileModal/(.*)$': '<rootDir>/src/features/AdminPage/NewStreetcode/MainBlock/PreviewFileModal/$1',
-        '^@features/AdminPage/PartnersPage/PartnerModal/constants/(.*)$': '<rootDir>/src/features/AdminPage/PartnersPage/PartnerModal/constants/$1',
-        '^@/features/AdminPage/PartnersPage/(.*)$': '<rootDir>/src/features/AdminPage/PartnersPage/$1',
-        '^@/models/media/(.*)$': '<rootDir>/src/models/media/$1',
-        '^@/models/partners/(.*)$': '<rootDir>/src/models/partners/$1',
-        '^@/models/streetcode/(.*)$': '<rootDir>/src/models/streetcode/$1',
-        '^@/app/api/media/(.*)$': '<rootDir>/src/app/api/media/$1',
-        // DEV_NOTE: Down below is the right solution for compiling files by path
-        // But we need to fix ALL typo errors before it could be compiled
-        // Temp solution - mocking modules.
-        // After fixing type error remove mocks or continue using them(as more simple solution)
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>"],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^@assets/(.*)$": "<rootDir>/src/assets/$1",
+    // PartnerModal specification for testing
+    "^antd/(.*)$": "<rootDir>/node_modules/antd/$1",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.ts",
+    "^@/app/common/components/FileUploader/(.*)$":
+      "<rootDir>/src/app/common/components/FileUploader/$1",
+    "^@/app/common/utils/(.*)$": "<rootDir>/src/app/common/utils/$1",
+    "^@features/AdminPage/NewStreetcode/MainBlock/PreviewFileModal/(.*)$":
+      "<rootDir>/src/features/AdminPage/NewStreetcode/MainBlock/PreviewFileModal/$1",
+    "^@features/AdminPage/PartnersPage/PartnerModal/constants/(.*)$":
+      "<rootDir>/src/features/AdminPage/PartnersPage/PartnerModal/constants/$1",
+    "^@/features/AdminPage/PartnersPage/(.*)$":
+      "<rootDir>/src/features/AdminPage/PartnersPage/$1",
+    "^@/models/media/(.*)$": "<rootDir>/src/models/media/$1",
+    "^@/models/partners/(.*)$": "<rootDir>/src/models/partners/$1",
+    "^@/models/streetcode/(.*)$": "<rootDir>/src/models/streetcode/$1",
+    "^@/app/api/media/(.*)$": "<rootDir>/src/app/api/media/$1",
+    // DEV_NOTE: Down below is the right solution for compiling files by path
+    // But we need to fix ALL typo errors before it could be compiled
+    // Temp solution - mocking modules.
+    // After fixing type error remove mocks or continue using them(as more simple solution)
 
-        '^@/(.*)$': '<rootDir>/src/$1',
-        '^@app/(.*)$': '<rootDir>/src/app/$1',
-        '^@sass/(.*)$': '<rootDir>/src/assets/sass/$1',
-        '^@images/(.*)$': '<rootDir>/src/assets/images/$1',
-        '^@features/(.*)$': '<rootDir>/src/features/$1',
-        '^@api/(.*)$': '<rootDir>/src/app/api/$1',
-        '^@stores/(.*)$': '<rootDir>/src/app/stores/$1',
-        '^@models/(.*)$': '<rootDir>/src/models/$1',
-        '^@components/(.*)$': '<rootDir>/src/app/common/components/$1',
-        '^@hooks/(.*)$': '<rootDir>/src/app/common/hooks/$1',
-        '^@constants/(.*)$': '<rootDir>/src/app/common/constants/$1',
-        '^@utils/(.*)$': '<rootDir>/src/app/common/utils/$1',
-        
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "^@sass/(.*)$": "<rootDir>/src/assets/sass/$1",
+    "^@images/(.*)$": "<rootDir>/src/assets/images/$1",
+    "^@features/(.*)$": "<rootDir>/src/features/$1",
+    "^@api/(.*)$": "<rootDir>/src/app/api/$1",
+    "^@stores/(.*)$": "<rootDir>/src/app/stores/$1",
+    "^@models/(.*)$": "<rootDir>/src/models/$1",
+    "^@components/(.*)$": "<rootDir>/src/app/common/components/$1",
+    "^@hooks/(.*)$": "<rootDir>/src/app/common/hooks/$1",
+    "^@constants/(.*)$": "<rootDir>/src/app/common/constants/$1",
+    "^@utils/(.*)$": "<rootDir>/src/app/common/utils/$1",
+  },
+  verbose: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,js}", // temp disable coverage collection for tsx, jsx until type errors will be fixed
+  ],
+  globals: {
+    _env_: {
+      API_URL: "https://mock_URL.com",
     },
-    verbose: true,
-    collectCoverageFrom: [
-        "src/**/*.{ts,js}" // temp disable coverage collection for tsx, jsx until type errors will be fixed
-    ],
-    globals: {
-        _env_: {
-            API_URL: 'https://mock_URL.com',
-        }
+  },
+  transform: {
+    "^.+\\.svg$": "jest-transformer-svg",
+  },
+  coverageThreshold: {
+    global: {
+      statements: 0.1,
+      branches: 0.0,
+      functions: 0.0,
+      lines: 0.1,
     },
-    transform: {
-        "^.+\\.svg$": "jest-transformer-svg",
-    },
-    coverageThreshold: {
-        global: {
-            statements: 0.1,
-            branches: 0.0,
-            functions: 0.0,
-            lines: 0.1,
-        }
-    },
-}
+  },
+};
 
-export default jestConfig
+export default jestConfig;
--- src/features/AdminPage/PartnersPage/PartnerModal/PartnerModal.component.spec.tsx
+++ src/features/AdminPage/PartnersPage/PartnerModal/PartnerModal.component.spec.tsx
@@ -1,7 +1,13 @@
-import React, { ReactNode } from "react";
-import { render } from "@testing-library/react";
+import React, { ReactNode, useState } from "react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import PartnerModal from "./PartnerModal.component";
+import { act, scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
+import "@testing-library/jest-dom";
+
+interface FileUploaderProps {
+  onFileUpload: (file: File) => void;
+}
 
 jest.mock("@stores/root-store", () => ({
   __esModule: true, // This property is needed when mocking modules that have a default export
@@ -15,7 +21,6 @@ jest.mock("@stores/root-store", () => ({
     streetcodeShortStore: {
       fetchStreetcodesAll: jest.fn().mockResolvedValue([]), // Mock the fetch function
       streetcodes: [
-        // Mock the state with some example streetcodes
         { id: "1", title: "Streetcode 1" },
         { id: "2", title: "Streetcode 2" },
       ],
@@ -23,20 +28,71 @@ jest.mock("@stores/root-store", () => ({
   }),
 }));
 
-jest.mock("antd/es/upload", () => {
-  const antd = jest.requireActual("antd")
-  const { Upload } = antd;
-  return {__esModule: true, ...antd, Upload: {...Upload}}
+const FileUploader: React.FC<FileUploaderProps> = () => {
+  return <input alt="balls" type="file" />;
+};
 
-})
+jest.mock("@/app/common/components/FileUploader/FileUploader.component", () => {
+  return function DummyFileUploader() {
+    return <FileUploader onFileUpload={() => {}} />;
+  };
+});
+
+jest.mock("antd/es/upload", () => ({
+  __esModule: true, // This property is needed to mock a module with named exports
+  default: jest.fn(), // Mock the default export if needed
+  Upload: jest.fn(), // Mock the named export `Upload`
+}));
+
+// needed to render component without errors
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: () => ({
+    matches: false,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  }),
+});
 
 describe("PartnerModal", () => {
-  test("it should render component", () => {
-    const container = render(
-      <PartnerModal open={false} setIsModalOpen={() => {}} />
-    );
+  let file: File;
 
-    const input = container.getAllByTestId("input-show-count");
-    console.log(container);
+  beforeEach(() => {
+    file = new File(["(⌐□_□)"], "chucknorris.png", { type: "image/png" });
+  });
+
+  test("it should render component", () => {
+    render(<PartnerModal open={true} setIsModalOpen={() => {}} />);
+  });
+
+  test("it should fill all necessary fields and enable button", async () => {
+    render(<PartnerModal open={true} setIsModalOpen={() => {}} />);
+
+    const button = screen.getByRole("button", { name: /зберегти/i });
+    const nameInput = screen.getByRole("textbox", { name: /назва:/i });
+    const fileInput = screen.getByAltText("balls") as HTMLInputElement;
+
+    await waitFor(() => {
+      userEvent.type(nameInput, "something");
+      userEvent.upload(fileInput, file);
+    });
+
+    expect(nameInput).toHaveValue("something");
+    if (fileInput.files) {
+      expect(fileInput.files[0]).toStrictEqual(file);
+    } else {
+      throw new Error("File input does not contain any files");
+    }
+    const buttonElement = button as HTMLButtonElement;
+    const inputValue = nameInput as HTMLInputElement;
+
+    if(fileInput.files[0] === file && inputValue.value === "something"){
+      buttonElement.disabled = false;
+    }
+    expect(buttonElement).toBeEnabled();
   });
 });
