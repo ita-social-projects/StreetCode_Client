943282e35a5983106007d3f67ebb844bacd56312
IvannYad <yad7devadress@gmail.com>
2024-04-06 14:27:31 +0300

    feat: started changing user-login-store

--- src/app/stores/user-login-store.ts
+++ src/app/stores/user-login-store.ts
@@ -1,95 +1,96 @@
-import { makeAutoObservable } from 'mobx';
+/* eslint-disable class-methods-use-this */
+import { action, makeObservable } from 'mobx';
 import UserApi from '@api/user/user.api';
 
-import { RefreshTokenResponce, UserLoginResponce } from '@/models/user/user.model';
+import User, { RefreshTokenResponce, UserLoginResponse } from '@/models/user/user.model';
 
 export default class UserLoginStore {
-
-    private timeoutHandler: NodeJS.Timeout | undefined;
-
     private static tokenStorageName = 'token';
 
-    private static dateStorageName = 'expireAt';
-
-    public userLoginResponce?: UserLoginResponce;
+    public user?: User;
 
     private callback?:()=>void;
 
     public constructor() {
-        makeAutoObservable(this);
+        makeObservable(this, {
+            login: action,
+        });
     }
 
-    private static getExpiredDate():number {
-        return Number(localStorage.getItem(UserLoginStore.dateStorageName)!);
-    }
-
-    private static setExpiredDate(date: string):void {
-        localStorage.setItem(UserLoginStore.dateStorageName, date);
+    private setUser(newUser: User) {
+        this.user = newUser;
     }
 
     public static getToken() {
-        return localStorage.getItem(UserLoginStore.tokenStorageName);
+        return localStorage.getItem(this.tokenStorageName);
     }
 
-    public static setToken(newToken:string) {
-        return localStorage.setItem(UserLoginStore.tokenStorageName, newToken);
+    private static setToken(newToken:string) {
+        return localStorage.setItem(this.tokenStorageName, newToken);
     }
 
     private static clearToken() {
-        localStorage.removeItem(UserLoginStore.tokenStorageName);
+        localStorage.removeItem(this.tokenStorageName);
     }
 
     public setCallback(func:()=>void) {
         this.callback = func;
     }
 
-    public static get isLoggedIn():boolean {
-        return UserLoginStore.getExpiredDate() > new Date(Date.now()).getTime();
-    }
+    // public clearUserData() {
+    //     if (this.timeoutHandler) {
+    //         clearTimeout(this.timeoutHandler);
+    //     }
+    //     localStorage.removeItem(UserLoginStore.tokenStorageName);
+    //     localStorage.removeItem(UserLoginStore.dateStorageName);
+    // }
 
-    public clearUserData() {
-        if (this.timeoutHandler) {
-            clearTimeout(this.timeoutHandler);
-        }
-        localStorage.removeItem(UserLoginStore.tokenStorageName);
-        localStorage.removeItem(UserLoginStore.dateStorageName);
-    }
+    // public logout() {
+    //     this.clearUserData();
+    // }
 
-    public logout() {
-        this.clearUserData();
-    }
+    // public setUserLoginResponce(user:UserLoginResponce) {
+    //     try {
+    //         const timeNumber = (new Date(user.expireAt)).getTime();
+    //         UserLoginStore.setExpiredDate(timeNumber.toString());
+    //         const expireForSeconds = timeNumber - new Date().getTime();
+    //         this.userLoginResponce = user;
+    //         UserLoginStore.setToken(user.token);
+    //         if (expireForSeconds > 10000) {
+    //             this.timeoutHandler = setTimeout(() => {
+    //                 if (this.callback) {
+    //                     this.callback();
+    //                 }
+    //             }, expireForSeconds - 10000);
+    //         }
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // }
 
-    public setUserLoginResponce(user:UserLoginResponce, func:()=>void) {
-        try {
-            const timeNumber = (new Date(user.expireAt)).getTime();
-            UserLoginStore.setExpiredDate(timeNumber.toString());
-            const expireForSeconds = timeNumber - new Date().getTime();
-            this.setCallback(func);
-            this.userLoginResponce = user;
-            UserLoginStore.setToken(user.token);
-            if (expireForSeconds > 10000) {
-                this.timeoutHandler = setTimeout(() => {
-                    if (this.callback) {
-                        this.callback();
-                    }
-                }, expireForSeconds - 10000);
-            }
-        } catch (e) {
-            console.log(e);
-        }
+    public async login(login: string, password: string) {
+        await UserApi.login({ login, password })
+            .then((response: UserLoginResponse) => {
+                UserLoginStore.clearToken();
+                UserLoginStore.setToken(response.token);
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+                return Promise.reject(error);
+            });
     }
-
-    public refreshToken = ():Promise<RefreshTokenResponce> => (
-        UserApi.refreshToken({ token: UserLoginStore.getToken() ?? '' })
-            .then((refreshToken) => {
-                const expireForSeconds = (new Date(refreshToken.expireAt)).getTime() - new Date().getTime();
-                this.timeoutHandler = setTimeout(() => {
-                    if (this.callback) {
-                        this.callback();
-                    }
-                }, expireForSeconds);
-                UserLoginStore.setExpiredDate((new Date(refreshToken.expireAt)).getTime().toString());
-                UserLoginStore.setToken(refreshToken.token);
-                return refreshToken;
-            }));
+    // public refreshToken = ():Promise<RefreshTokenResponce> => (
+    //     UserApi.refreshToken({ token: UserLoginStore.getToken() ?? '' })
+    //         .then((refreshToken) => {
+    //             const expireForSeconds = (new Date(refreshToken.expireAt)).getTime() - new Date().getTime();
+    //             this.timeoutHandler = setTimeout(() => {
+    //                 if (this.callback) {
+    //                     this.callback();
+    //                 }
+    //             }, expireForSeconds);
+    //             UserLoginStore.setExpiredDate((new Date(refreshToken.expireAt)).getTime().toString());
+    //             UserLoginStore.setToken(refreshToken.token);
+    //             return refreshToken;
+    //         }));
 }
--- src/features/AdminPage/Login/AdminLogin.component.tsx
+++ src/features/AdminPage/Login/AdminLogin.component.tsx
@@ -1,50 +1,60 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable import/extensions */
+/* eslint-disable no-underscore-dangle */
 import './AdminLogin.style.scss';
 
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import { Navigate, useNavigate } from 'react-router-dom';
 
 import { Button, Form, Input, message } from 'antd';
 
 import UserApi from '@/app/api/user/user.api';
+import { ERROR_MESSAGES } from '@/app/common/constants/error-messages.constants';
 import FRONTEND_ROUTES from '@/app/common/constants/frontend-routes.constants';
 import useMobx, { useModalContext } from '@/app/stores/root-store';
 import UserLoginStore from '@/app/stores/user-login-store';
-import { UserLoginResponce } from '@/models/user/user.model';
 
 const AdminLogin:React.FC = () => {
-    const { modalStore } = useModalContext();
     const { userLoginStore } = useMobx();
     const navigate = useNavigate();
     const [form] = Form.useForm();
+    const [messageApi, messageContextHolder] = message.useMessage({ maxCount: 3 });
+    const [isVerified, setIsVerified] = useState(false);
+    const recaptchaRef = useRef<ReCAPTCHA>(null);
+    const siteKey = window._env_.RECAPTCHA_SITE_KEY;
+    const { MESSAGE_LIMIT, SOMETHING_IS_WRONG, RECAPTCHA_CHECK } = ERROR_MESSAGES;
 
-    const setConfirmationModal = () => {
-        modalStore.setConfirmationModal('confirmation', () => {
-            userLoginStore.refreshToken()
-                .catch((e) => {});
-            modalStore.setConfirmationModal('confirmation');
-        }, 'Бажаєте продовжити сеанс?', undefined, () => {
-            userLoginStore.clearUserData();
-        });
+    const handleVerify = () => {
+        setIsVerified(true);
     };
 
-    const onSuccessfulLogin = (loginResponce: UserLoginResponce) => userLoginStore
-        .setUserLoginResponce(loginResponce, setConfirmationModal);
+    const handleExpiration = () => {
+        setIsVerified(false);
+    };
 
-    const login = (formValues:any) => {
-        UserApi.login({ login: formValues.login, password: formValues.password })
-            .then((logResp) => onSuccessfulLogin(logResp))
-            .then(() => navigate(FRONTEND_ROUTES.ADMIN.BASE))
-            .catch((er) => {
-                message
-                    .error('Неправильний логін чи пароль');
-            });
+    const handleLogin = async ({ login, password }: any) => {
+        try {
+            await userLoginStore.login(login, password);
+        } catch (error) {
+            message.error(
+                'Неправильний логін чи пароль',
+            );
+        }
+        // UserApi.login({ login: formValues.login, password: formValues.password })
+        //     .then((logResp) => onSuccessfulLogin(logResp))
+        //     .then(() => navigate(FRONTEND_ROUTES.ADMIN.BASE))
+        //     .catch((er) => {
+        //         message
+        //             .error('Неправильний логін чи пароль');
+        //     });
     };
     if (UserLoginStore.isLoggedIn) {
         return <Navigate to={FRONTEND_ROUTES.ADMIN.BASE} />;
     }
 
     return (
-        <Form form={form} className="admin-login-form" onFinish={login}>
+        <Form form={form} className="admin-login-form" onFinish={handleLogin}>
             <Form.Item name="login" label="Логін" rules={[{ required: true, message: 'Введіть логін' }]}>
                 <Input maxLength={20} />
             </Form.Item>
@@ -52,6 +62,15 @@ const AdminLogin:React.FC = () => {
             <Form.Item name="password" label="Пароль" rules={[{ required: true, message: 'Введіть пароль' }]}>
                 <Input.Password maxLength={20} />
             </Form.Item>
+            <div className="captchaBlock">
+                <ReCAPTCHA
+                    className="required-captcha"
+                    sitekey={siteKey}
+                    onChange={handleVerify}
+                    onExpired={handleExpiration}
+                    ref={recaptchaRef}
+                />
+            </div>
             <Form.Item>
                 <Button htmlType="submit" className="streetcode-custom-button">
                     Увійти
