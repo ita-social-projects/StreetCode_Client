f1d3294d0320a1825ca741568b37d94f14c623e6
IvannYad <yad7devadress@gmail.com>
2024-04-06 14:27:31 +0300

    feat: started changing user-login-store

--- src/models/user/user.model.ts
+++ src/models/user/user.model.ts
@@ -1,10 +1,8 @@
 export default interface User {
-    id:number;
     name:string;
     surname:string;
     email:string;
     login:string;
-    password:string;
     userRole:UserRole;
 }
 
@@ -12,10 +10,9 @@ export interface UserLoginRequest {
     login:string;
     password:string;
 }
-export interface UserLoginResponce {
+export interface UserLoginResponse {
     user:User;
     token:string;
-    expireAt:Date;
 }
 
 export interface RefreshTokenRequest {
@@ -28,7 +25,6 @@ export interface RefreshTokenResponce {
 }
 
 export enum UserRole {
-    MainAdministrator,
-    Administrator,
-    Moderator,
+    Admin,
+    User,
 }
