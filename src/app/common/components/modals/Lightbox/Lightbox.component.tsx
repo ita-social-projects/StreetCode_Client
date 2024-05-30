8aae514caf7a9307107e50292a327ead4db6a20d
Spoilsport <nastasiyka.ver@gmail.com>
2024-05-30 23:17:18 +0300

    change font size and font family

--- src/app/common/components/modals/Lightbox/Lightbox.component.tsx
+++ src/app/common/components/modals/Lightbox/Lightbox.component.tsx
@@ -1,63 +1,70 @@
 import './LightboxComponent.styles.scss';
-
 import { observer } from 'mobx-react-lite';
 import { useMemo, useState } from 'react';
 import { useIdleTimer } from 'react-idle-timer';
 import useMobx, { useModalContext } from '@stores/root-store';
 import Lightbox from 'yet-another-react-lightbox';
 import Captions from 'yet-another-react-lightbox/plugins/captions';
-
 import base64ToUrl from '@/app/common/utils/base64ToUrl.utility';
-
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
 import 'yet-another-react-lightbox/plugins/captions.css';
 import 'yet-another-react-lightbox/styles.css';
 
-const LightboxComponent = () => {
-    const { artStore: { arts, mutationObserved } } = useMobx();
-    const { modalStore } = useModalContext();
-    const { setModal, modalsState: { artGallery: { isOpen, fromCardId } } } = modalStore;
-
-    const [isCaptionEnabled, setIsCaptionEnabled] = useState(true);
-
-    const slides = useMemo(() => arts.map(
-        ({ image: { base64, mimeType }, description, title }, index) => {
-          let src = base64ToUrl(base64, mimeType);
-          if (!src) {
-            src = ''; //replace with default src
-          }
-          return {
-            src,
-            title: `${index + 1}/${arts.length}`,
-            description: `${title ?? ''}. \n\n${description ?? ''}`,
-          };
-        }
-      ), [mutationObserved]);
-      
-
-    const currentArtIndex = useMemo(() => arts.findIndex(
-        (art) => art.id === fromCardId,
-    ), [mutationObserved, fromCardId]);
-
-    const onIdleTimerHandlers = useMemo(() => ({
-        onIdle: () => setIsCaptionEnabled(false),
-        onActive: () => setIsCaptionEnabled(true),
-    }), []);
-
-    useIdleTimer({
-        ...onIdleTimerHandlers,
-        timeout: 3_000,
-    });
-
-    return (
-        <Lightbox
-            open={isOpen}
-            close={() => setModal('artGallery')}
-            index={currentArtIndex}
-            className={`lightbox ${!isCaptionEnabled ? 'lightboxDisabled' : ''}`}
-            slides={slides}
-            plugins={[Captions]}
-        />
-    );
+const TitleDescriptionComponent = ({ title, description }: { title: string| undefined, description: string| undefined }) => {
+  return (
+    <div>
+      <div className="title">{title}</div>
+      <div className="description">{description}</div>
+    </div>
+  );
 };
+
+
+const LightboxComponent = () => {
+  const { artStore: { arts, mutationObserved } } = useMobx();
+  const { modalStore } = useModalContext();
+  const { setModal, modalsState: { artGallery: { isOpen, fromCardId } } } = modalStore;
+
+  const [isCaptionEnabled, setIsCaptionEnabled] = useState(true);
+
+  const slides = useMemo(() =>
+    arts.map(({ image: { base64, mimeType }, description, title }, index) => {
+      let src = base64ToUrl(base64, mimeType);
+      if (!src) {
+        src = ''; //replace with default src
+      }
+
+      return {
+        src,
+        title: `${index + 1}/${arts.length}`,
+        description: <TitleDescriptionComponent title={title} description={description} />
+      };
+    }), [mutationObserved]);
+
+  const currentArtIndex = useMemo(() => arts.findIndex(
+    (art) => art.id === fromCardId,
+  ), [mutationObserved, fromCardId]);
+
+  const onIdleTimerHandlers = useMemo(() => ({
+    onIdle: () => setIsCaptionEnabled(false),
+    onActive: () => setIsCaptionEnabled(true),
+  }), []);
+
+  useIdleTimer({
+    ...onIdleTimerHandlers,
+    timeout: 3_000,
+  });
+
+  return (
+    <Lightbox
+      open={isOpen}
+      close={() => setModal('artGallery')}
+      index={currentArtIndex}
+      className={`lightbox ${!isCaptionEnabled ? 'lightboxDisabled' : ''}`}
+      slides={slides}
+      plugins={[Captions]}
+    />
+  );
+};
+
 export default observer(LightboxComponent);
