f73702d4a3a1f84f06ee3857dc980e478fd2f434
Anton <anton260903@gmail.com>
2024-08-04 14:53:15 +0300

    feat: wow facts slider fixed for 2 and 3 slides

--- src/features/StreetcodePage/InterestingFactsBlock/InterestingFacts.component.tsx
+++ src/features/StreetcodePage/InterestingFactsBlock/InterestingFacts.component.tsx
@@ -32,10 +32,7 @@ const InterestingFactsComponent = () => {
                     Promise.all(res.map((f, index) => ImagesApi.getById(f.imageId).then((img) => {
                         res[index].image = img;
                     }))).then(() => {
-                        setSliderArray(res.length === 3
-                            || res.length === 2
-                            ? res.concat(res)
-                            : res);
+                        setSliderArray(res);
                     });
                 });
             }
@@ -70,11 +67,12 @@ const InterestingFactsComponent = () => {
 
     const setings = {
         initialSlide: initialSlideIndex,
-        dots: facts.current.length > 3,
+        dots: sliderArray.length > 1,
         swipeOnClick: false,
         touchThreshold: 25,
         rtl: false,
         centerMode: true,
+        ...((sliderArray.length === 2 || sliderArray.length === 3) && { slidesToShow: sliderArray.length - 0.01 }),
         infinite: sliderArray.length > 1,
         swipe: false,
         centerPadding: '-5px',
@@ -100,59 +98,67 @@ const InterestingFactsComponent = () => {
                 },
             },
             {
+                breakpoint: sliderArray.length === 2 ? 968 : 0,
+                settings: {
+                    swipe: true,
+                    arrows: false,
+                },
+            },
+            {
                 breakpoint: 1025,
                 settings: {
                     centerPadding: '-27.5px',
-                    arrows: false,
-                    swipe: true,
+                    arrows: sliderArray.length === 2,
+                    swipe: sliderArray.length !== 2,
                 },
             },
         ],
     };
     return (
         <div>
-            {facts.current.length > 0
-                ? (
-                    <div
-                        id="wow-facts"
-                        className={`container "interestingFactsWrapper"`}
-                    >
-                        <BlockHeading headingText="Wow-факти" />
-                        <div className={`interestingFactsContainer
-                    ${facts.current.length === 1 ? 'oneFactContainer' : ''}`}
-                        >
-                            <div className="interestingFactsSliderContainer">
-                                <div style={{ height: '100%' }}>
-                                    {(facts.current.length === 1) ? (
-                                        <div className="oneFactItem"
-                                        onClick={handleModalOpen}
-                                        onKeyDown={handleModalOpen}>
+            {facts.current.length > 0 ? (
+                <div
+                    id="wow-facts"
+                    className={`container "interestingFactsWrapper"`}
+                >
+                    <BlockHeading headingText="Wow-факти" />
+                    <div className={`interestingFactsContainer ${sliderArray.length === 1 ? "oneFactContainer" : ""}`} >
+                        <div className="interestingFactsSliderContainer">
+                            <div style={{ height: "100%" }}
+                                 className={`${sliderArray.length === 3? 'slides-3': ''} ${sliderArray.length === 2? 'slides-2': ''}`} 
+                            >
+                                {facts.current.length === 1 ? (
+                                    <div className="oneFactItem"
+                                         onClick={handleModalOpen}
+                                         onKeyDown={handleModalOpen}
+                                    >
+                                        <InterestingFactItem
+                                            fact={facts.current[0]}
+                                            middleFactIndex={middleFactIndex}
+                                        />
+                                    </div>
+                                ) : (
+                                    <BlockSlider
+                                        className="heightContainer"
+                                        {...setings}
+                                    >
+                                        {sliderArray.map((fact, index) => (
                                             <InterestingFactItem
-                                                fact={facts.current[0]}
+                                                key={fact.id}
+                                                fact={fact}
+                                                index={index}
                                                 middleFactIndex={middleFactIndex}
                                             />
-                                        </div>
-                                    ) : (
-                                        <BlockSlider
-                                            className="heightContainer"
-                                            {...setings}
-                                        >
-                                            {sliderArray.map((fact, index) => (
-                                                <InterestingFactItem
-                                                    key={fact.id}
-                                                    fact={fact}
-                                                    index={index}
-                                                    middleFactIndex={middleFactIndex}
-                                                />
-                                            ))}
-                                        </BlockSlider>
-                                    )}
-                                </div>
+                                        ))}
+                                    </BlockSlider>
+                                )}
                             </div>
                         </div>
                     </div>
-                )
-                : <></>}
+                </div>
+            ) : (
+                <></>
+            )}
         </div>
     );
 };
