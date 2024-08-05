c86684a44b7805104f55fb8208e18fde1b2d677d
DDeviak <117632082+DDeviak@users.noreply.github.com>
2024-08-05 22:08:39 +0300

    chore: Refactored uniquenessValidator to handle undefined initial value)

--- src/app/common/utils/uniquenessValidator.ts
+++ src/app/common/utils/uniquenessValidator.ts
@@ -2,7 +2,7 @@
 import normaliseWhitespaces from './normaliseWhitespaces';
 
 const uniquenessValidator = (getAll:()=>string[], getInitial:()=>string|undefined, message?:string) => (async (rule: any, value: string) => new Promise<void>((resolve, reject) => {
-    const normalizedValue = normaliseWhitespaces(value).trim();
+    const normalizedValue = normaliseWhitespaces(value)?.trim();
     if (!!value && getAll().includes(normalizedValue) && normalizedValue !== getInitial()) {
         reject(message || 'Value already exists');
     } else {
