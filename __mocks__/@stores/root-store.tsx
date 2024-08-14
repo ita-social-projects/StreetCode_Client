32d29f5139a03ad1598887cb3ebe62b8db5ff207
DDeviak <117632082+DDeviak@users.noreply.github.com>
2024-08-15 00:28:57 +0300

    feat: Extended root store mock

--- __mocks__/@stores/root-store.tsx
+++ __mocks__/@stores/root-store.tsx
@@ -1,7 +1,10 @@
-import Context, {ContextCreate} from '@models/additional-content/context.model';
+import Context, { ContextCreate } from '@models/additional-content/context.model';
 import ContextsApi from '@api/additional-content/contexts.api';
 import TeampositionsApi from '@api/additional-content/teampositions.api';
 import Position from '@models/additional-content/teampositions.model';
+import { SourceCategoryAdmin } from '@/models/sources/sources.model';
+import sourcesApi from '@/app/api/sources/sources.api';
+import SourceCreateUpdateStreetcode from '@/app/stores/source-category-store-create';
 
 export const mockID = 1;
 export const mockIsOpen = true;
@@ -21,7 +24,7 @@ export const useModalContext = () => ({
             },
         },
         useModalContext: mockUseModalContext,
-        setConfirmationModal: jest.fn((modalName : any, onSubmit: () => void) => {}),
+        setConfirmationModal: jest.fn((modalName: any, onSubmit: () => void) => { }),
     },
 });
 
@@ -83,6 +86,22 @@ export const useMobx = () => ({
             },
         ],
     },
+    sourcesAdminStore: {
+        setSource: jest.fn(),
+        setInternalSourceCategories: jest.fn(),
+        getSourcesAdmin: [],
+        fetchSourceCategories: jest.fn(),
+        deleteSourceCategory: jest.fn(async (srcId: number) => {
+            await sourcesApi.delete(srcId);
+        }),
+        addSourceCategory: jest.fn(async (sourceItem: SourceCategoryAdmin) => {
+            await sourcesApi.create(sourceItem);
+        }),
+        updateSourceCategory: jest.fn(async (sourceItem: SourceCategoryAdmin) => {
+            await sourcesApi.update(sourceItem);
+        }),
+    },
+    sourceCreateUpdateStreetcode: new SourceCreateUpdateStreetcode(),
 });
 
 export default useMobx;
