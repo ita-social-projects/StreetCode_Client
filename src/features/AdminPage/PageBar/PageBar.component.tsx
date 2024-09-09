30c6d61e023527b3f80f435a1287f530f95f9234
ulyasaur <ulyano4ka2003@gmail.com>
2024-09-09 16:16:46 +0300

    fix resetting streetcode filters after clicking "Стріткоди" button

--- src/features/AdminPage/PageBar/PageBar.component.tsx
+++ src/features/AdminPage/PageBar/PageBar.component.tsx
@@ -6,12 +6,12 @@ import FRONTEND_ROUTES from '@constants/frontend-routes.constants';
 const PageBar = () => (
     <div className="PageBarContainer">
         <div className="BarContainer">
-            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.BASE}>Стріткоди</Link>
-            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.PARTNERS}>Партнери</Link>
-            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.EDITOR}>Едітор</Link>
-            <Link className="Link" to={`${FRONTEND_ROUTES.ADMIN.TEAM}`}>Команда</Link>
-            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.NEWS}>Новини</Link>
-            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.JOBS}>Вакансії</Link>
+            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.BASE} reloadDocument>Стріткоди</Link>
+            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.PARTNERS} reloadDocument>Партнери</Link>
+            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.EDITOR} reloadDocument>Едітор</Link>
+            <Link className="Link" to={`${FRONTEND_ROUTES.ADMIN.TEAM}`} reloadDocument>Команда</Link>
+            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.NEWS} reloadDocument>Новини</Link>
+            <Link className="Link" to={FRONTEND_ROUTES.ADMIN.JOBS} reloadDocument>Вакансії</Link>
         </div>
     </div>
 );
