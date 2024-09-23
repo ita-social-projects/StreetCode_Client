8871463a35927ba8dd4aea232f9d2733597d9ef9
Anton <anton260903@gmail.com>
2024-09-23 21:41:43 +0300

    feat: some refactoring

--- src/features/MainPage/StaticBanners/StaticBanner.component.tsx
+++ src/features/MainPage/StaticBanners/StaticBanner.component.tsx
@@ -1,9 +1,7 @@
 import './StaticBanner.styles.scss';
 
 import useWindowSize from '@/app/common/hooks/stateful/useWindowSize.hook';
-
 import Ornament from '@/assets/images/main-page/ornament.svg';
-
 
 interface Props {
     id: 'catalog' | 'support';
@@ -17,12 +15,10 @@ const StaticBanner = ({
     id, blockName, blockContent, buttonName, setActionOnClick,
 }: Props) => (
     <div>
-        {useWindowSize().width <= 480 && id === 'support' ? <div className="top" /> : <></>}
-
+        {useWindowSize().width <= 480 && id === 'support' ? <div className="top" /> : <> </>}
 
         <div className={`mainPageBlockStaticBanner ${id}`}>
-            {useWindowSize().width > 768 ? <Ornament className="left" /> : <></>}
-
+            {useWindowSize().width > 768 ? <Ornament className="left" /> : <> </>}
             <div className="mainContainer">
                 <div className="textContainer">
                     <p className="title">{blockName}</p>
@@ -32,9 +28,9 @@ const StaticBanner = ({
                     <p>{buttonName}</p>
                 </div>
             </div>
-            {useWindowSize().width > 768 ? <Ornament className="right" /> : <></>}
+            {useWindowSize().width > 768 ? <Ornament className="right" /> : <> </>}
         </div>
-        {useWindowSize().width <= 480 && id === 'support' ? <div className="bottom" /> : <></>}
+        {useWindowSize().width <= 480 && id === 'support' ? <div className="bottom" /> : <> </>}
     </div>
 
 );
