b3cdd2d64f5502eb89b2f9e69208e56e439e8a3a
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2024-12-17 19:04:36 +0200

    feat: fix incorrect redirection on donate button click

--- src/app/api/donates/donation.api.ts
+++ src/app/api/donates/donation.api.ts
@@ -1,10 +1,10 @@
 import Agent from '@api/agent.api';
 import { API_ROUTES } from '@constants/api-routes.constants';
 
-import Donation from '@/models/feedback/donation.model';
+import Donation, { DonationResponse } from '@/models/feedback/donation.model';
 
 const DonationApi = {
-    create: (donation: Donation) => Agent.post<Donation>(`${API_ROUTES.DONATION.CREATE}`, donation),
+    create: (donation: Donation) : Promise<DonationResponse> => Agent.post<DonationResponse>(`${API_ROUTES.DONATION.CREATE}`, donation),
 };
 
 export default DonationApi;
