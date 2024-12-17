1a850ede311c4ecab3d42feb6c434c47cd5247e3
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2024-12-17 19:12:20 +0200

    code analysis fixes

--- src/app/api/donates/donation.api.ts
+++ src/app/api/donates/donation.api.ts
@@ -4,7 +4,8 @@ import { API_ROUTES } from '@constants/api-routes.constants';
 import Donation, { DonationResponse } from '@/models/feedback/donation.model';
 
 const DonationApi = {
-    create: (donation: Donation) : Promise<DonationResponse> => Agent.post<DonationResponse>(`${API_ROUTES.DONATION.CREATE}`, donation),
+    create: (donation: Donation)
+        : Promise<DonationResponse> => Agent.post<DonationResponse>(`${API_ROUTES.DONATION.CREATE}`, donation),
 };
 
 export default DonationApi;
