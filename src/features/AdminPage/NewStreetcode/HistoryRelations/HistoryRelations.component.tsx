6d036821aa1d511b4e99d96295ff1943b553cc60
lisaaksionova bc2024-6 <lisaaksionova@gmail.com>
2024-12-28 17:04:20 +0200

    import path changed to correct value

--- src/features/AdminPage/NewStreetcode/HistoryRelations/HistoryRelations.component.tsx
+++ src/features/AdminPage/NewStreetcode/HistoryRelations/HistoryRelations.component.tsx
@@ -1,67 +1,86 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ModelState } from '@models/enums/model-state';
-import { RelatedFigureCreateUpdate, RelatedFigureShort } from '@models/streetcode/related-figure.model';
+import {
+    RelatedFigureCreateUpdate,
+    RelatedFigureShort,
+} from '@models/streetcode/related-figure.model';
 
 import StreetcodesApi from '@/app/api/streetcode/streetcodes.api';
-import Streetcode, { StreetcodeShort } from '@/models/streetcode/streetcode-types.model';
+import Streetcode, {
+    StreetcodeShort,
+} from '@/models/streetcode/streetcode-types.model';
 
-import InputPanel from './components/nent';
+import InputPanel from './components/InputPanel.component';
 import RelationsList from './components/RelatedFigureList.component';
 
 interface Props {
     currentStreetcodeId: number | null;
     figures: RelatedFigureCreateUpdate[];
-    setFigures: React.Dispatch<React.SetStateAction<RelatedFigureCreateUpdate[]>>;
+    setFigures: React.Dispatch<
+        React.SetStateAction<RelatedFigureCreateUpdate[]>
+    >;
     onChange: (field: string, value: any) => void;
 }
 
-const RelatedFiguresBlock = React.memo(({ currentStreetcodeId, figures, setFigures, onChange }: Props) => {
-    const [options, setOptions] = useState<StreetcodeShort[]>([]);
+const RelatedFiguresBlock = React.memo(
+    ({ currentStreetcodeId, figures, setFigures, onChange }: Props) => {
+        const [options, setOptions] = useState<StreetcodeShort[]>([]);
 
-    const handleAdd = (relationToAdd: RelatedFigureCreateUpdate) => {
-        const figurePersisted = figures.find((rel) => rel.id === relationToAdd.id);
-        if (figurePersisted) { // for case when delete persisted item and add it again
-            figurePersisted.modelState = ModelState.Updated;
-            setFigures([...figures]);
-        } else {
-            const figure = options.find((rel) => rel.id === relationToAdd.id) as RelatedFigureCreateUpdate;
-            figure.modelState = ModelState.Created;
-            setFigures((prevState) => [...prevState, figure]);
-        }
-    };
+        const handleAdd = (relationToAdd: RelatedFigureCreateUpdate) => {
+            const figurePersisted = figures.find(
+                (rel) => rel.id === relationToAdd.id
+            );
+            if (figurePersisted) {
+                // for case when delete persisted item and add it again
+                figurePersisted.modelState = ModelState.Updated;
+                setFigures([...figures]);
+            } else {
+                const figure = options.find(
+                    (rel) => rel.id === relationToAdd.id
+                ) as RelatedFigureCreateUpdate;
+                figure.modelState = ModelState.Created;
+                setFigures((prevState) => [...prevState, figure]);
+            }
+        };
 
-    const handleDelete = async (id: number) => {
-        const figureToDelete = figures.find((x) => x.id === id);
-        if (figureToDelete?.isPersisted) {
-            figureToDelete.modelState = ModelState.Deleted;
-            setFigures([...figures]);
-        } else {
-            const newRelatedSCs = figures.filter((rel) => rel.id !== id);
-            setFigures(newRelatedSCs);
-        }
-        onChange('figures', figures);
-    };
+        const handleDelete = async (id: number) => {
+            const figureToDelete = figures.find((x) => x.id === id);
+            if (figureToDelete?.isPersisted) {
+                figureToDelete.modelState = ModelState.Deleted;
+                setFigures([...figures]);
+            } else {
+                const newRelatedSCs = figures.filter((rel) => rel.id !== id);
+                setFigures(newRelatedSCs);
+            }
+            onChange('figures', figures);
+        };
 
-    const getOptions = async () => {
-        Promise.all([
-            StreetcodesApi.getAllPublished().then((ops) => {
-                setOptions(ops.filter((x) => x.id !== currentStreetcodeId));
-            }),
-        ]);
-    };
+        const getOptions = async () => {
+            Promise.all([
+                StreetcodesApi.getAllPublished().then((ops) => {
+                    setOptions(ops.filter((x) => x.id !== currentStreetcodeId));
+                }),
+            ]);
+        };
 
-    useEffect(() => {
-        getOptions();
-    }, []);
+        useEffect(() => {
+            getOptions();
+        }, []);
 
-    return (
-        <div className="adminContainer-block">
-            <h2>Зв&apos;язки історії(History-коди)</h2>
-            <InputPanel figures={figures} options={options} handleAdd={handleAdd} onChange={onChange} />
-            <RelationsList figures={figures} handleDelete={handleDelete} />
-        </div>
-    );
-});
+        return (
+            <div className='adminContainer-block'>
+                <h2>Зв&apos;язки історії(History-коди)</h2>
+                <InputPanel
+                    figures={figures}
+                    options={options}
+                    handleAdd={handleAdd}
+                    onChange={onChange}
+                />
+                <RelationsList figures={figures} handleDelete={handleDelete} />
+            </div>
+        );
+    }
+);
 
 export default RelatedFiguresBlock;
