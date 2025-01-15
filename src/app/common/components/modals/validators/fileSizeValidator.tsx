160a6e3afe47b4cb33a0348c6255085d8518b908
JuliaAndreyeva <blackonblackua@gmail.com>
2025-01-15 13:24:59 +0200

    fix: updated file size validator with condition after removing photo

--- src/app/common/components/modals/validators/fileSizeValidator.tsx
+++ src/app/common/components/modals/validators/fileSizeValidator.tsx
@@ -1,29 +1,25 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { RuleObject } from 'rc-field-form/lib/interface';
 
+import { UploadFileStatus } from 'antd/es/upload/interface';
+
 import { MaxFileSizeMB } from '@/app/common/constants/enums/file-size.enum';
 
 const fileSizeValidator = (maxFileSize: MaxFileSizeMB) => (_: RuleObject, file: any): Promise<void> => {
-    if (file) {
-        const maxSizeInBytes = maxFileSize * 1024 * 1024;
-        let size = 0;
+    if (file?.file?.status === 'removed' as UploadFileStatus) {
+        return Promise.resolve();
+    }
 
-        if (file.file) {
-            size = file.file.size;
-        } else if (file.size) {
-            size = file.size;
-        }
+    const maxSizeInBytes = maxFileSize * 1024 * 1024;
+    const fileSize = file.file?.size || file.size || 0;
 
-        if (size <= maxSizeInBytes) {
-            return Promise.resolve();
-        }
-
+    if (fileSize > maxSizeInBytes) {
         return Promise.reject(
             new Error(`Розмір файлу не має бути більше ${maxFileSize} MB!`),
         );
     }
 
-    return Promise.reject(new Error('Не вдалося перевірити розмір файлу!'));
+    return Promise.resolve();
 };
 
 export default fileSizeValidator;
