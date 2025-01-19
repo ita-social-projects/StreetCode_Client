e4b3cfd167c5e4cac3902feb371463a47911fd9d
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2025-01-19 02:50:12 +0200

    added new APIs

---
+++ src/app/api/users/users.api.ts
@@ -0,0 +1,17 @@
+﻿/* eslint-disable no-underscore-dangle */
+import Agent from '@api/agent.api';
+
+import { API_ROUTES } from '@/app/common/constants/api-routes.constants';
+import User, { UpdateUser } from '@/models/user/user.model';
+
+const UsersApi = {
+    getByUserName: () => (
+        Agent.get<User>(API_ROUTES.USERS.GET_BY_ID)
+    ),
+    update: (user : UpdateUser) => (
+        Agent.put<User>(API_ROUTES.USERS.UPDATE, user)
+    ),
+    delete: (email : string) => Agent.delete(`${API_ROUTES.USERS.DELETE}/${email}`),
+
+};
+export default UsersApi;
