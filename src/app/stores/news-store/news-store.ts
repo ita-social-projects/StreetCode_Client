3ece1d739b8a163977a0d1977eb882c9a03adad2
vlad-zhadan <zhadan.vld@gmail.com>
2025-01-23 11:18:17 +0200

    fix bug with the date not converting properly because of mobx reactivity

--- src/app/stores/news-store/news-store.ts
+++ src/app/stores/news-store/news-store.ts
@@ -43,11 +43,11 @@ export default class NewsStore {
         const localOffset = new Date().getTimezoneOffset() / 60;
         this.NewsMap.clear();
         if (news) {
-            news.forEach(this.addNews);
             news.forEach((n) => {
                 // eslint-disable-next-line no-param-reassign
                 n.creationDate = dayjs(n.creationDate).subtract(localOffset, 'hours');
             });
+            news.forEach(this.addNews);
         }
     }
 
