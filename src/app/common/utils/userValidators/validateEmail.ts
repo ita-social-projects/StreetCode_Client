d661d0516d2765156c37aab729b17e56878f1066
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2025-01-29 22:18:55 +0200

    fixed eslint warnings

--- src/app/common/utils/userValidators/validateEmail.ts
+++ src/app/common/utils/userValidators/validateEmail.ts
@@ -1,6 +1,8 @@
 ﻿import { EMAIL_REGEX } from '@constants/regex.constants';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { RuleObject } from 'rc-field-form/lib/interface';
 
-const validateEmail = (_: any, value: string) => {
+const validateEmail = (_: RuleObject, value: string) => {
     if (!value || EMAIL_REGEX.test(value)) {
         return Promise.resolve();
     }
