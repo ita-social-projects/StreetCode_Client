08ad6517d3796e307bb244c3cf81d982c045a3bc
Ihor Yatsyshyn <ihoryatsyshyn4@gmail.com>
2025-04-22 22:14:13 +0300

    Fixed test problem

--- src/features/AdminPage/ContextPage/ContextMainPage.component.spec.tsx
+++ src/features/AdminPage/ContextPage/ContextMainPage.component.spec.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import BUTTON_LABELS from '@constants/buttonLabels';
+import { act } from '@testing-library/react';
 
 jest.mock('antd', () => {
     const antd = jest.requireActual('antd');
@@ -22,8 +23,30 @@ jest.mock('antd', () => {
 });
 
 jest.mock('mobx-react-lite', () => ({
-    ...jest.requireActual('mobx-react-lite'), // if you want to keep some original functionality
-    observer: jest.fn((Component) => Component), // mock observer as a pass-through
+    ...jest.requireActual('mobx-react-lite'),
+    observer: jest.fn((Component) => Component),
+}));
+
+jest.mock('@stores/root-store', () => ({
+    __esModule: true,
+    useModalContext: jest.fn(() => ({
+        modalStore: {
+            setConfirmationModal: jest.fn(),
+        },
+    })),
+    useMobx: jest.fn(() => ({
+        contextStore: {
+            setCurrentPage: jest.fn(),
+            fetchContexts: jest.fn(),
+            deleteContext: jest.fn(),
+            getContextArray: [],
+            PaginationInfo: {
+                CurrentPage: 1,
+                PageSize: 10,
+                TotalItems: 0,
+            },
+        },
+    })),
 }));
 
 export default function overrideMatchMedia() {
@@ -48,57 +71,67 @@ describe('ContextMainPage', () => {
     beforeEach(() => {
         jest.resetAllMocks();
     });
-    
-    const queryClient = new QueryClient();
 
-    it('should render component without crashes', () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <ContextMainPage/>
-            </QueryClientProvider>,
-        );
-        const column1 = screen.getByRole('columnheader', {name: /назва/i});
-        const column2 = screen.getByRole('columnheader', {name: /дії/i});
-        const context1 = screen.getByText(/революція гідності/i);
-        const context2 = screen.getByText(/студентство/i);
-        const button = screen.getByText(/революція гідності/i);
+    const queryClient = new QueryClient();
+    const mockModalStore = {
+        setConfirmationModal: jest.fn(),
+    };
+
+    it('should render component without crashes', async () => {
+        await act(async () => {
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <ContextMainPage />
+                </QueryClientProvider>,
+            );
+        });
+
+        const column1 = screen.getByRole('columnheader', { name: /назва/i });
+        const column2 = screen.getByRole('columnheader', { name: /дії/i });
+
         expect(column1).toBeInTheDocument();
         expect(column2).toBeInTheDocument();
-        expect(context1).toBeInTheDocument();
-        expect(context2).toBeInTheDocument();
-        expect(button).toBeInTheDocument();
     });
 
-    it('opens the add modal on button click', () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <ContextMainPage/>
-            </QueryClientProvider>,
-        );
+    it('opens the add modal on button click', async () => {
+        await act(async () => {
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <ContextMainPage/>
+                </QueryClientProvider>,
+            );
+        });
+
         const addButton = screen.getByText(BUTTON_LABELS.ADD_CONTEXT);
-        userEvent.click(addButton);
+        await userEvent.click(addButton); 
+
         const button = screen.getByText(BUTTON_LABELS.SAVE);
         const label = screen.getByText(/назва:/i);
         const title = screen.getByRole('heading', { name: /додати контекст/i });
+
         expect(button).toBeInTheDocument();
         expect(title).toBeInTheDocument();
         expect(label).toBeInTheDocument();
     });
 
-    it('open edits context', () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <ContextMainPage/>
-            </QueryClientProvider>,
-        );
-        const addButton = screen.getByText(BUTTON_LABELS.ADD_CONTEXT);
-        userEvent.click(addButton);
-        const row = screen.getByRole('row', { name: /революція гідності/i });
-        const button = within(row).getByRole('img', { name: /edit/i });
-        userEvent.click(button);
+    it('open edits context', async () => {
+        await act(async () => {
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <ContextMainPage/>
+                </QueryClientProvider>,
+            );
+        });
+
+        const row = screen.getByRole('row', { name: /Революція гідності/i });
+        const editButton = within(row).getByRole('button', { name: /edit/i });
+
+        await userEvent.click(editButton);
+
         const title = screen.getByRole('heading', { name: /редагувати контекст/i });
-        const characterAmout = screen.getByText(/18 \/ 50/i);
+        const characterAmount = screen.getByText(/18 \/ 50/i);
+
         expect(title).toBeInTheDocument();
-        expect(characterAmout).toBeInTheDocument();
+        expect(characterAmount).toBeInTheDocument();
     });
 });
